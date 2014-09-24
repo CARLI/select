c48247677eda210b913964e14ce22c5a29fe5b08
Brian and Bryan <carli@pixotech.com>
2014-09-24 15:38:38 -0500

    CARLI-11 Copy test results out of container

--- jenkins/run-test-container.sh
+++ jenkins/run-test-container.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-docker run --rm --workdir=/carli-select carli-grunt grunt test
+docker run --name carli-build --workdir=/carli-select carli-grunt grunt test
+mkdir -p artifacts/test-results
+docker cp carli-build:/carli-select/artifacts/test-results/karma.xml artifacts/test-results
+docker rm carli-build
+
