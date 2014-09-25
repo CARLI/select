1eedad587c2ab31c11bbb89a77926ecd469d28ad
Bryan Cribbs <bryan@pixotech.com>
2014-09-25 14:37:12 -0500

    CARLI-11 Skip exporting the container
    It is unnecesarry to export the container right now, since carli.dev and
    carli.qa are both going to be hosted on the same machine that is doing
    the builds for now.

--- jenkins/jobs/CARLI-build.sh
+++ jenkins/jobs/CARLI-build.sh
@@ -7,4 +7,9 @@ SCRIPTS=./jenkins/scripts
 
 $SCRIPTS/build-grunt-container.sh
 $SCRIPTS/run-test-container.sh
-$SCRIPTS/export-grunt-container.sh
+
+# It isn't necessary to export the image right now, because we 
+# are serving both carli.dev and carli.qa directly from the same
+# machine that does the builds.
+
+# $SCRIPTS/export-grunt-container.sh
