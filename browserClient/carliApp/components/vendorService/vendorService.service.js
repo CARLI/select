412bf214ae4447853f8929cec965b15b44fa29f2
Marty and Dan <marty@pixotech.com>
2014-10-08 11:35:38 -0500

    Added get vendor list to Vendor service

--- browserClient/carliApp/components/vendorService/vendorService.service.js
+++ browserClient/carliApp/components/vendorService/vendorService.service.js
@@ -3,7 +3,13 @@ angular.module('carli.vendorService')
 
 function vendorService($resource) {
     var service = {};
-    var vendorResource = $resource('/resources/vendorList.json');
-    service.getVendors = vendorResource.query;
+    var vendorListResource = $resource('/resources/vendorList.json');
+
+    service.getVendorList = vendorListResource.query;
+    service.getVendor = function( vendorId ){
+        var vendorResource = $resource('/resources/vendors/'+vendorId+'/vendor.json');
+        return vendorResource.get();
+    };
+
     return service;
 }
