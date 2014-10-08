56e2f5e0d5c9a2407153c836dc3a731cad107011
Brian and Bryan <bryan@pixotech.com>
2014-10-08 11:04:35 -0500

    Fix broken test

--- browserClient/carliApp/components/cycleStatus/cycleStatus.spec.js
+++ browserClient/carliApp/components/cycleStatus/cycleStatus.spec.js
@@ -2,7 +2,5 @@ describe('The Cycle Status Component', function(){
 
     beforeEach(module('carli.cycleStatus'));
 
-    it('should ', inject(function(Carli){
-        //expect(Carli).to.be.an('object');
-    }));
+    it('should do stuff');
 });
