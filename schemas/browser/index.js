1ff4b5691071554bedb73bdc437780c80c02b0dc
Bryan and Marty <marty@pixotech.com>
2014-10-10 11:13:42 -0500

    CARLI-19 Resolve refs links when starting docson server

--- schemas/browser/index.js
+++ schemas/browser/index.js
@@ -1,8 +1,25 @@
-
+var fs = require('fs');
 var static = require('node-static');
 var file = new static.Server('.');
 
 var port = 8001;
+
+function processJsonFile( fileName ) {
+    var fileContents = fs.readFileSync('../'+fileName, { encoding: 'utf8'} );
+
+    jsonRefRegex = /"\$ref":\s*"\.\/(.*)\.json"/g;
+
+    newFileContents = fileContents.replace( jsonRefRegex, '"$ref": "http://localhost:'+port+'/$1.json"' );
+
+    fs.writeFileSync( fileName, newFileContents );
+}
+
+var fileList = fs.readdirSync('../');
+for ( var i = 0 ; i < fileList.length ; i++ ) {
+    if ( fileList[i].slice(-5) == '.json' ) {
+        processJsonFile( fileList[i] );
+    }
+}
 
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
