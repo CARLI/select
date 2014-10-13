8e1a2701b3aa68bc5a968ae1d2f182f4a8d688e0
Dan & Marty <dans@pixotech.com>
2014-10-13 11:39:08 -0500

    Added extraction for individual vendors and contacts

--- migration/vendor.js
+++ migration/vendor.js
@@ -4,6 +4,7 @@
 var CONFIG = require('./config');
 var fs = require('fs');
 var mysql = require('mysql');
+var mkdirp = require('mkdirp');
 
 // establish the connection
 var connection = mysql.createConnection( CONFIG.dsn );
@@ -28,14 +29,104 @@ function createJsonFiles(vendors) {
 
     var vendorsFilePath = 'data/vendor/list.json';
     var vendorList = [];
-    fs.mkdirSync('data/vendor');
+    var vendorData;
+
+    mkdirp('data/vendor');
 
     for (var i in vendors) {
-        // TODO: write out each vendor in an individual file in this loop
-        vendorList.push( { id: vendors[i].id, name: vendors[i].name, isActive: true})
+        var v = vendors[i];
 
+        vendorList.push({id: v.id, name: v.name, isActive: true})
+
+        createVendorJsonFile(v);
     }
 
-    fs.writeFileSync( vendorsFilePath, JSON.stringify(vendorList, null, 2));
+    fs.writeFileSync(vendorsFilePath, JSON.stringify(vendorList, null, 2));
+}
 
+function createVendorJsonFile(v) {
+    vendorFilePath = 'data/vendor/' + v.id;
+    mkdirp(vendorFilePath);
+
+    vendorData = {
+        id: v.id,
+        name: v.name,
+        previousName: "",
+        webSite: v.info_url,
+        contacts: extractVendorContacts(v),
+        comments: "",
+        adminModule: v.admin_module_url,
+        isActive: true,
+        licenseAgreements: extractVendorLicenseAgreements(v),
+        products: extractVendorProducts(v)
+    }
+
+    fs.writeFileSync(vendorFilePath + "/data.json", JSON.stringify(vendorData, null, 2));
+
+}
+
+function extractVendorContacts(vendor) {
+
+    contactList = [];
+    if (vendor.salesrep_name) {
+        contactList.push(extractSalesContact(vendor));
+    }
+    if (vendor.salesrep2_name) {
+        contactList.push(extractSalesContact2(vendor));
+    }
+    if (vendor.technicalcontact_name) {
+        contactList.push(extractTechnicalContact(vendor));
+    }
+    if (vendor.technicalcontact2_name) {
+        contactList.push(extractTechnicalContact2(vendor));
+    }
+    return contactList;
+}
+
+function extractSalesContact(vendor) {
+    return {
+        firstName: vendor.salesrep_name,
+        lastName: "",
+        email: vendor.salesrep_email,
+        phoneNumber: vendor.salesrep_phone,
+        contactType: "Sales"
+    };
+}
+
+function extractSalesContact2(vendor) {
+    return {
+        firstName: vendor.salesrep2_name,
+        lastName: "",
+        email: vendor.salesrep2_email,
+        phoneNumber: vendor.salesrep2_phone,
+        contactType: "Sales"
+    };
+}
+
+function extractTechnicalContact(vendor) {
+    return {
+        firstName: vendor.technicalcontact_name,
+        lastName: "",
+        email: vendor.technicalcontact_email,
+        phoneNumber: vendor.technicalcontact_phone,
+        contactType: "Technical"
+    };
+}
+
+function extractTechnicalContact2(vendor) {
+    return {
+        firstName: vendor.technicalcontact2_name,
+        lastName: "",
+        email: vendor.technicalcontact2_email,
+        phoneNumber: vendor.technicalcontact2_phone,
+        contactType: "Technical"
+    };
+}
+
+function extractVendorLicenseAgreements(vendor) {
+    return [];
+}
+
+function extractVendorProducts(vendor) {
+    return [];
 }
