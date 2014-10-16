bfdb9329d84495df0d5892b16a5cc3fbd38b3923
Bryan and Marty <bryan@pixotech.com>
2014-10-16 10:36:36 -0500

    CARLI-67 Example of how to run test in 2 browsers

--- browserClient/protractor.conf.js
+++ browserClient/protractor.conf.js
@@ -1,4 +1,12 @@
 exports.config = {
-      seleniumAddress: 'http://localhost:4444/wd/hub',
-        specs: ['carliApp/sections/**/*.spec.js']
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['carliApp/sections/**/*.spec.js'],
+    multiCapabilities: [
+        {
+            'browserName': 'firefox'
+        },
+        {
+            'browserName': 'chrome'
+        }
+    ]
 };
