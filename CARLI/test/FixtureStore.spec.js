d4afd7807c560c4e487b47f74b4b2adff21068bd
Marty and Wes <marty@pixotech.com>
2014-10-17 11:20:20 -0500

    (ub)

--- CARLI/test/FixtureStore.spec.js
+++ CARLI/test/FixtureStore.spec.js
@@ -1,7 +1,4 @@
-var chai   = require( 'chai' )
-  , expect = chai.expect
-  , uuid   = require( 'node-uuid' )
-  , test   = require( './StoreInterface.spec' )
+var test   = require( './StoreInterface.spec' )
 ;
 
 test.run( 'FixtureStore' );
