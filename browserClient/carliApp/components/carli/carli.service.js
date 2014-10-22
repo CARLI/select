4bfb125794bca4caba4a84d6f3179600af7d3143
Marty and Brian <marty@pixotech.com>
2014-10-22 10:29:33 -0500

    Added some more tests for the CARLI Service

--- browserClient/carliApp/components/carli/carli.service.js
+++ browserClient/carliApp/components/carli/carli.service.js
@@ -1,14 +1,14 @@
 /**
- * @name Carli Service
+ * @name CARLI Service
  * @ngdoc service
  * @description 
  * ## External Code ##
  * This service exists solely to import the packaged CARLI code into the Angular app.
  * All the code under CARLI/ is bundled using Browserify and exposed via a global variable
- * **window.Carli**, which this service returns. 
+ * **window.CARLI**, which this service returns. 
  *
  */
 angular.module('carli.service')
-.factory('Carli', function(){
-    return window.Carli;
+.factory('CARLI', function(){
+    return window.CARLI;
 });
--- browserClient/carliApp/components/carli/carli.spec.js
+++ browserClient/carliApp/components/carli/carli.spec.js
@@ -1,8 +1,20 @@
-describe('The Carli Service', function(){
+describe('The CARLI Service', function(){
 
     beforeEach( module('carli.service') );
 
-    it( 'should provide the Carli service', inject( function(Carli) {
+    it( 'should provide the CARLI module', inject( function(CARLI) {
         expect(CARLI).to.be.an('Object');
+    }));
+
+    it( 'should export the Vendor module', inject( function(CARLI) {
+        expect(CARLI.Vendor).to.be.an('Object');
+    }));
+
+    it( 'should export the Store interface', inject( function(CARLI) {
+        expect(CARLI.Store).to.be.a('Function');
+    }));
+
+    it( 'should export the FixtureStore module', inject( function(CARLI) {
+        expect(CARLI.FixtureStore).to.be.an('Object');
     }));
 });
