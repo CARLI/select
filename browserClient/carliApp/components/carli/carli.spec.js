b67d7bc080892c063fb0547c769b1679493e4c7a
Marty and Brian <marty@pixotech.com>
2014-10-22 10:24:07 -0500

    Changed the name of the global CARLI object

--- browserClient/carliApp/components/carli/carli.spec.js
+++ browserClient/carliApp/components/carli/carli.spec.js
@@ -3,6 +3,6 @@ describe('The Carli Service', function(){
     beforeEach( module('carli.service') );
 
     it( 'should provide the Carli service', inject( function(Carli) {
-        expect(Carli).to.be.an('Object');
+        expect(CARLI).to.be.an('Object');
     }));
 });
