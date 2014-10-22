fd6359bbc252a83e688cf7ef75bd0e1186537940
Marty and Brian <marty@pixotech.com>
2014-10-22 10:46:12 -0500

    Renamed files to match the service name

--- browserClient/carliApp/components/carli/carliModules.service.js
+++ browserClient/carliApp/components/carli/carliModules.service.js
@@ -8,7 +8,7 @@
  * **window.CARLI**, which this service returns. 
  *
  */
-angular.module('carli.service')
+angular.module('carli.carliModules.service')
 .factory('CarliModules', function( $window ){
     return $window.CARLI;
 });
