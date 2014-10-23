db6f6989a542076dbbd5e67d08207ae484058cb5
Bryan and Dan <bryan@pixotech.com>
2014-10-23 11:05:46 -0500

    CARLI-58 top level task runner
    `grunt test` - Runs tests in subdirs
    `grunt install-deps` - Runs both npm-install and bower-install tasks
    `grunt npm-install` - Runs `npm install` in subdirs
    `grunt bower-install` - Runs `bower install` in subdirs
    
    Note: Only browserClient and CARLI are used for now, since the other
    directories are non-essential (docs and schemas).

---
+++ Gruntfile.js
@@ -0,0 +1,69 @@
+module.exports = function(grunt) {
+
+    grunt.registerTask('subdir-grunt', subdirGruntTask);
+    grunt.registerTask('subdir-exec', subdirExecTask);
+
+    grunt.registerTask('test', function() {
+        grunt.task.run(['subdir-grunt:browserClient:test']);
+        grunt.task.run(['subdir-exec:CARLI:npm test']);
+    });
+
+    grunt.registerTask('npm-install', function() {
+        grunt.task.run(['subdir-exec:browserClient:npm install']);
+        grunt.task.run(['subdir-exec:CARLI:npm install']);
+    });
+    grunt.registerTask('bower-install', function() {
+        grunt.task.run(['subdir-exec:browserClient:bower install']);
+    });
+
+    grunt.registerTask('install-deps', [
+        'npm-install',
+        'bower-install'
+    ]);
+
+    function subdirGruntTask(dir, task) {
+        var done = this.async();
+        var spawnArgs = {
+            grunt: true,
+            args: [ task ],
+            opts: {
+                cwd: dir,
+                stdio: 'inherit'
+            }
+        };
+
+        grunt.log.writeln('* Entering ' + dir);
+        grunt.util.spawn(spawnArgs, spawnCallback(dir, done));
+    }
+    function subdirExecTask(dir, commandString) {
+        var done = this.async();
+
+        var args = commandString.split(' ');
+        var command = args.shift();
+
+        var spawnArgs = {
+            cmd: command,
+            args:  args,
+            opts: {
+                cwd: dir,
+                stdio: 'inherit'
+            }
+        };
+
+        grunt.log.writeln('* Entering ' + dir);
+        grunt.util.spawn(spawnArgs, spawnCallback(dir, done));
+    }
+
+    function spawnCallback(dir, done) {
+        return function (err, result, code) {
+            if (err == null) {
+                grunt.log.writeln('* Finished ' + dir);
+                done();
+            }
+            else {
+                grunt.log.writeln('! ' + dir + ' failed: ' + code);
+                done(false);
+            }
+        };
+    }
+};
