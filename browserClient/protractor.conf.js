4fa2d05ed2bf1b13ff1c36de7073f5c376872dc3
Marty Kane <marty@pixotech.com>
2014-10-23 12:50:04 -0500

    Add nicer reporter for Protractor

--- browserClient/protractor.conf.js
+++ browserClient/protractor.conf.js
@@ -4,5 +4,12 @@ var seleniumPort = process.env.SELENIUM_PORT_4444_TCP_PORT || '4444';
 exports.config = {
     seleniumAddress: 'http://' + seleniumHost + ':' + seleniumPort + '/wd/hub',
     /* TODO: should come from build.config.js */
-    specs: ['carliApp/*.spec.js','carliApp/sections/**/*.spec.js']
+    specs: ['carliApp/*.spec.js','carliApp/sections/**/*.spec.js'],
+    onPrepare: function() {
+        require('jasmine-spec-reporter');
+        jasmine.getEnv().addReporter(new jasmine.SpecReporter({displayStacktrace: true}));
+    },
+    jasmineNodeOpts: {
+        silent: true
+    }
 };
