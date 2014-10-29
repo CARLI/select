0ab85f0e6cd9f30d9c7f9944184348a83658a032
Marty and Dan <dans@pixotech.com>
2014-10-29 14:06:15 -0500

    CARLI-94 Added schema for Library and requirements.

--- schemas/browser/test.js
+++ schemas/browser/test.js
@@ -2,24 +2,17 @@
 var fs = require('fs');
 var tv4 = require('tv4');
 
-function addSchema(filename) {
+function validateSchema(filename) {
     var schema = JSON.parse(fs.readFileSync(filename, 'utf8'));
     tv4.addSchema(filename, schema);
     return schema;
 }
 
-var contactSchema = addSchema('contact.json');
-var cycleTypeSchema = addSchema('cycle-type.json');
-var priceCapSchema = addSchema('price-cap.json');
-var productSchema = addSchema('product.json');
-var urlSchema = addSchema('web-address.json');
-var vendorSchema = addSchema('vendor.json');
+var fileList = fs.readdirSync('./');
+fileList = fileList.filter(function (file) {
+    return ( file.slice(-5) == '.json' );
+});
 
-var data = {
-    'name': 'Acme',
-    'websiteUrl': 'http://acme.com'
-};
-
-var result = tv4.validateResult(data, vendorSchema);
-console.log(result);
-
+fileList.map( function(file){
+    validateSchema( file );
+});
