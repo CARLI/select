f7dab8ee6549034db95cac32fe04a76e6ae0a9cf
Bryan and Brian W <bryan@pixotech.com>
2014-10-29 17:03:07 -0500

    CARLI-88 Vendor list styles

--- browserClient/carliApp/sections/vendors/vendors.controller.js
+++ browserClient/carliApp/sections/vendors/vendors.controller.js
@@ -1,9 +1,35 @@
 angular.module('carli.sections.vendors')
 .controller('vendorsController', vendorController);
 
+var testVendors = [
+    {"name":"ACME Publishing","contacts":[],"websiteUrl":"http://www.acme.com","isActive":true,"comments":"foobar"},
+    {"name":"ACME Publishing","contacts":[],"websiteUrl":"http://www.acme.com","isActive":true,"comments":"foobar"},
+    {"name":"ACME Publishing","contacts":[],"websiteUrl":"http://www.acme.com","isActive":true,"comments":"foobar"},
+    {"name":"ACME Publishing","contacts":[],"websiteUrl":"http://www.acme.com","isActive":true,"comments":"foobar"},
+    {"name":"ACME Publishing","contacts":[],"websiteUrl":"http://www.acme.com","isActive":true,"comments":"foobar"},
+    {"name":"ACME Publishing","contacts":[],"websiteUrl":"http://www.acme.com","isActive":true,"comments":"foobar"},
+    {"name":"ACME Publishing","contacts":[],"websiteUrl":"http://www.acme.com","isActive":true,"comments":"foobar"},
+    {"name":"FOOBAR Publishing","contacts":[],"websiteUrl":"http://www.acme.com","isActive":true,"comments":"acme"},
+    {"name":"ACME Publishing","contacts":[],"websiteUrl":"http://www.acme.com","isActive":true,"comments":"foobar"},
+    {"name":"ACME Publishing","contacts":[],"websiteUrl":"http://www.acme.com","isActive":true,"comments":"foobar"},
+    {"name":"ACME Publishing","contacts":[],"websiteUrl":"http://www.acme.com","isActive":true,"comments":"foobar"}
+];
+
 function vendorController( vendorService ){
     var vm = this;
+    createTestVendors();
+    activate();
 
-    vm.vendorList = vendorService.list();
-    vm.activeFilterState = 'Active';
+
+    function activate() {
+        vm.vendorList = vendorService.list();
+        vm.activeFilterState = 'Active';
+    }
+
+    function createTestVendors() {
+        testVendors.forEach(function (v) {
+            vendorService.create(v);
+        });
+    }
 }
+
