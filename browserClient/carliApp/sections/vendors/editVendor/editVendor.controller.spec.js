6cfb454d91decc74e497c519084f2fd5b606c14b
Lonesome Marty <marty@pixotech.com>
2014-10-30 09:15:33 -0500

    CARLI-89 Test create or update. Cleaned up the mock dependencies.

--- browserClient/carliApp/sections/vendors/editVendor/editVendor.controller.spec.js
+++ browserClient/carliApp/sections/vendors/editVendor/editVendor.controller.spec.js
@@ -1,37 +1,56 @@
 
 describe('The Edit Vendor Controller', function(){
 
-    var mockDependenciesForNewVendor = {
-        $location: {},
-        $routeParams: {
-            id: 'new'
-        },
-        vendorService: {}
+    var mockLocation = {
+        path: function(){}
     };
 
-    var mockDependenciesForEditVendor = {
-        $location: {},
-        $routeParams: {
-            id: 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'
+    var mockVendorService = {
+        createOrUpdate: 'neither',
+        create: function(){
+            this.createOrUpdate = 'create';
         },
-        vendorService: {
-            load: function(){ 
-                return {
-                    name: 'Test Vendor',
-                    "contacts": [
-                        {
-                            "contactType": "Billing",
-                            "name": "Bob Martin",
-                            "email": "bob@cleancode.org",
-                            "phoneNumber": "123-555-1234"
-                        }
-                    ]
-                }; 
-            }
+        update: function(){
+            this.createOrUpdate = 'update';
+        },
+        load: function(){
+            return {
+                name: 'Test Vendor',
+                "contacts": [
+                    {
+                        "contactType": "Billing",
+                        "name": "Bob Martin",
+                        "email": "bob@cleancode.org",
+                        "phoneNumber": "123-555-1234"
+                    }
+                ]
+            };
+        },
+        reset: function(){
+            this.createOrUpdate = 'neither';
         }
     };
 
+    var mockDependenciesForNewVendor = {
+        $location: mockLocation,
+        $routeParams: {
+            id: 'new'
+        },
+        vendorService: mockVendorService
+    };
+
+    var mockDependenciesForEditVendor = {
+        $location: mockLocation,
+        $routeParams: {
+            id: 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'
+        },
+        vendorService: mockVendorService
+    };
+
     beforeEach(module('carli.sections.vendors.edit'));
+    afterEach(function(){
+        mockVendorService.reset();
+    });
 
     it('should have a default, editable Vendor on the New Vendor screen', inject(function($controller){
         var editCtrl = $controller('editVendorController', mockDependenciesForNewVendor);
@@ -39,10 +58,24 @@ describe('The Edit Vendor Controller', function(){
         expect( editCtrl.editable ).to.equal(true);
     }));
 
+    it('should call vendorService.create when saving a new Vendor', inject(function($controller){
+        var editCtrl = $controller('editVendorController', mockDependenciesForNewVendor);
+        expect( mockDependenciesForNewVendor.vendorService.createOrUpdate ).to.equal('neither');
+        editCtrl.saveVendor();
+        expect( mockDependenciesForNewVendor.vendorService.createOrUpdate ).to.equal('create');
+    }));
+
     it('should have a known, non-editable Vendor on the Edit Vendor screen', inject(function($controller){
         var editCtrl = $controller('editVendorController', mockDependenciesForEditVendor);
         expect( editCtrl.vendor.name ).to.equal('Test Vendor');
         expect( editCtrl.editable ).to.equal(false);
+    }));
+
+    it('should call vendorService.update when saving an existing Vendor', inject(function($controller){
+        var editCtrl = $controller('editVendorController', mockDependenciesForEditVendor);
+        expect( mockDependenciesForEditVendor.vendorService.createOrUpdate ).to.equal('neither');
+        editCtrl.saveVendor();
+        expect( mockDependenciesForEditVendor.vendorService.createOrUpdate ).to.equal('update');
     }));
 
     it('should toggle the editable variable when calling toggleEditable()', inject(function($controller){
