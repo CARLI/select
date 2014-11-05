ee9923995f6c813e689957d7b860121bd1678228
Marty and Brian <marty@pixotech.com>
2014-11-05 16:19:37 -0600

    Added select view edit components

--- browserClient/carliApp/sections/libraries/editLibrary/editLibrary.config.js
+++ browserClient/carliApp/sections/libraries/editLibrary/editLibrary.config.js
@@ -1,4 +1,8 @@
 angular.module('carli.sections.libraries.edit',[
     'carli.libraryService',
-    'carli.viewEditDirectives.viewEditContact'
+    'carli.viewEditDirectives.viewEditContact',
+    'carli.viewEditDirectives.viewEditRadios',
+    'carli.viewEditDirectives.viewEditSelect',
+    'carli.viewEditDirectives.viewEditTextArea',
+    'carli.viewEditDirectives.viewEditTextField'
 ]);
