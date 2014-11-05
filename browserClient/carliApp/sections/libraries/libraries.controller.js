2e2acb44fd36275687a981f88c0c7d1237c0a42d
Marty and Brian <marty@pixotech.com>
2014-11-05 15:48:08 -0600

    Move Library fixture data from controller to service

--- browserClient/carliApp/sections/libraries/libraries.controller.js
+++ browserClient/carliApp/sections/libraries/libraries.controller.js
@@ -1,32 +1,12 @@
 angular.module('carli.sections.libraries')
 .controller('librariesController', libraryController);
 
-var testLibraries = [
-    {"type":"Library", "name":"Test Library 1", "institutionYears":"2 Year", "institutionType":"Public", "isActive":true, "contacts":[]},
-    {"type":"Library", "name":"Test Library 2", "institutionYears":"2 Year", "institutionType":"Public", "contacts":[]},
-    {"type":"Library", "name":"Test Library your mom", "institutionYears":"2 Year", "institutionType":"Private", "contacts":[]},
-    {"type":"Library", "name":"Test Library 3", "institutionYears":"2 Year", "institutionType":"Public", "contacts":[]},
-    {"type":"Library", "name":"Test Library 4", "institutionYears":"4 Year", "institutionType":"Private", "contacts":[]},
-    {"type":"Library", "name":"Test Library 5", "institutionYears":"4 Year", "institutionType":"Public", "contacts":[]},
-    {"type":"Library", "name":"Test Library 6", "institutionYears":"2 Year", "institutionType":"Public", "contacts":[]},
-    {"type":"Library", "name":"Test Library Seven", "institutionYears":"2 Year", "institutionType":"Public", "contacts":[]},
-    {"type":"Library", "name":"Test Library 8", "institutionYears":"4 Year", "institutionType":"Public", "contacts":[]},
-    {"type":"Library", "name":"Test Library 9", "institutionYears":"4 Year", "institutionType":"Private", "contacts":[]}
-];
-
 function libraryController( $sce, libraryService ){
     var vm = this;
 
     vm.mostRecentFteUpdateDate = libraryService.getMostRecentFteUpdateDate();
     vm.saveAllLibraries = saveAllLibraries;
     vm.closeFteModal = closeFteModal;
-
-    function createTestLibraries() {
-        testLibraries.forEach(function (l) {
-            libraryService.create(l);
-        });
-    }
-    createTestLibraries();
 
     vm.libraryList = libraryService.list();
 
