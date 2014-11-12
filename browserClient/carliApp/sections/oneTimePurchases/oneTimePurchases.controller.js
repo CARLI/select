0b5fffce98cb14d72af5e59bdc1cdbb7652ed763
Dan and Marty <marty@pixotech.com>
2014-11-12 10:33:05 -0600

    Add skeletal structure for One-Time Purchases section

---
+++ browserClient/carliApp/sections/oneTimePurchases/oneTimePurchases.controller.js
@@ -0,0 +1,7 @@
+angular.module('carli.sections.oneTimePurchases')
+.controller('oneTimePurchasesController', oneTimePurchasesController);
+
+function oneTimePurchasesController( libraryService, produceService ){
+    var vm = this;
+
+}
