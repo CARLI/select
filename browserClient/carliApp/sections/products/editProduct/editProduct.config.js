4908bfe49a2cd5812bed838b8824acf673c2d4d1
Brian and Dan <dans@pixotech.com>
2014-11-12 14:51:48 -0600

    CARLI-95 Add term fields to product, using viewEditYesNoOther directive

--- browserClient/carliApp/sections/products/editProduct/editProduct.config.js
+++ browserClient/carliApp/sections/products/editProduct/editProduct.config.js
@@ -1,6 +1,7 @@
 angular.module('carli.sections.products.edit',[
     'carli.productService',
     'carli.viewEditDirectives.viewEditCheckbox',
+    'carli.viewEditDirectives.viewEditYesNoOther',
     'carli.viewEditDirectives.viewEditTextField',
     'carli.viewEditDirectives.viewEditTextArea',
     'carli.viewEditDirectives.viewEditRadios'
