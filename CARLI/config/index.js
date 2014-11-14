9bc2a0fed2212623dec95936abcd4a31bfe626da
Bryan and Wes <bryan@pixotech.com>
2014-11-14 13:13:32 -0600

    Add local config and generator for local container config

--- CARLI/config/index.js
+++ CARLI/config/index.js
@@ -1,8 +1,13 @@
-module.exports = {
+var _ = require('lodash')
+  , localConfig = require('./local');
+
+var defaults = {
     storeOptions: {
         couchDbUrl: 'http://localhost:5984',
-        couchDbName: 'testy',
+        couchDbName: 'testy'
     },
     request: require('../carliRequest'),
     store: 'CouchDbStore'
 };
+
+module.exports = _.extend(defaults, localConfig);
