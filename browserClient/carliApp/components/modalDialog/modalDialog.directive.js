7e0d295cc8cf38af5dabcca31ac2fb2b20692ed3
BDub and Dan <dans@pixotech.com>
2014-11-14 14:36:24 -0600

    Create ModalDialog as a general directive

---
+++ browserClient/carliApp/components/modalDialog/modalDialog.directive.js
@@ -0,0 +1,22 @@
+angular.module('carli.modalDialog')
+    .directive('modalDialog', function() {
+        return {
+            restrict: 'E',
+            templateUrl: '/carliApp/components/modalDialog/modalDialog.html',
+            scope: {
+                title: '=',
+                submitFn: '&',
+                id: '='
+            },
+            transclude: true,
+            controller: modalDialogController
+        };
+    });
+
+function modalDialogController($scope) {
+
+    $scope.closeDialog = function() {
+        $('#' + $scope.id).modal('hide');
+    };
+
+}
