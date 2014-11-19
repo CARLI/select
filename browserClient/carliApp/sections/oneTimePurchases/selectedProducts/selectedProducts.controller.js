36406414bfc74e486edf9103a9391f33b862a43f
Dan and Marty <marty@pixotech.com>
2014-11-19 13:28:22 -0600

    CARLI-143 Added buttons for purchasing and removing products.

--- browserClient/carliApp/sections/oneTimePurchases/selectedProducts/selectedProducts.controller.js
+++ browserClient/carliApp/sections/oneTimePurchases/selectedProducts/selectedProducts.controller.js
@@ -8,7 +8,6 @@ function selectedProductsController($routeParams, libraryService, productService
     vm.filterState = 'all';
 
     vm.filter = function( value ){
-        console.log( value );
         return (
             value.cycleType === 'One-Time Purchase' &&
             value.isActive === true &&
@@ -23,6 +22,33 @@ function selectedProductsController($routeParams, libraryService, productService
     vm.setShowAllProducts = function() {
         vm.filterState = 'all';
     };
+
+    vm.purchaseProduct = function(product) {
+        product.oneTimePurchase.libraryPurchaseData[vm.libraryId].datePurchased = new Date().toJSON().slice(0,10);
+        productService.update(product).then(function(){
+            //TODO: add notification of success (inline?)
+            //TODO: add audit trail entry (in service)
+        })
+        .catch(function(){
+            //TODO: notification of failure (inline?)
+            product.oneTimePurchase.libraryPurchaseData[vm.libraryId].datePurchased = null;
+        });
+    };
+
+    vm.cancelPurchase = function(product) {
+        var oldDate = product.oneTimePurchase.libraryPurchaseData[vm.libraryId].datePurchased;
+
+        product.oneTimePurchase.libraryPurchaseData[vm.libraryId].datePurchased = null;
+        productService.update(product).then(function(){
+            //TODO: add notification of success (inline?)
+            //TODO: add audit trail entry (in service)
+        })
+        .catch(function(){
+            //TODO: notification of failure (inline?)
+            product.oneTimePurchase.libraryPurchaseData[vm.libraryId].datePurchased = oldDate;
+        });
+    };
+
 
     activate();
 
