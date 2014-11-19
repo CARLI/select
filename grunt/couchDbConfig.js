e2ca574a300dfcd4b8e20eadb5d842222e903bf5
Han Solo <bryan@pixotech.com>
2014-11-19 10:50:36 -0600

    Factor out couch config into grunt module.
    Write tasks to generate different local configs for different ways of
    accessing couch (via the docker container subnet, versus the real LAN
    address).

---
+++ grunt/couchDbConfig.js
@@ -0,0 +1,67 @@
+var fs = require('fs');
+var _ = require('lodash');
+var localConfigFile = __dirname + '/../CARLI/config/local.js';
+
+module.exports = function (grunt) {
+    grunt.registerTask('generate-couch-config', generateCouchConfig);
+};
+
+function generateCouchConfig(instance) {
+    var cfg = readExistingConfig();
+    var storeOptions = cfg.storeOptions || {};
+
+    if (instance == 'test') {
+        cfg.storeOptions = _.extend(storeOptions, getContainerCouchConfig());
+    } else {
+        cfg.storeOptions = _.extend(storeOptions, getPublicCouchConfig(instance));
+    }
+
+    fs.writeFileSync(localConfigFile, JSON.stringify(cfg, null, '    '));
+}
+
+function readExistingConfig() {
+    var cfg;
+    try {
+        var cfgString = fs.readFileSync(localConfigFile, { encoding: 'utf-8' });
+        cfg = JSON.parse(cfgString);
+    } catch (e) {
+        cfg = {};
+    }
+    return cfg;
+}
+
+function getContainerCouchConfig() {
+    var host = process.env.COUCHDB_PORT_5984_TCP_ADDR;
+    var port = process.env.COUCHDB_PORT_5984_TCP_PORT;
+
+    if (host === undefined || port === undefined) {
+        throw new Error('Couch container link not found');
+    }
+
+    return _storeOptions('http://'+host+':'+port, 'testy');
+}
+
+function getPublicCouchConfig(instance) {
+    if (instance === undefined) {
+        instance = 'dev';
+    }
+    return _storeOptions(getPublicCouchDbUrlFor(instance), 'testy');
+}
+
+function getPublicCouchDbUrlFor(instance) {
+    switch (instance) {
+        case 'dev':
+            return 'http://carli-db.dev.pixotech.com';
+        case 'qa':
+            return 'http://carli-db.qa.pixotech.com';
+        default:
+            throw new Error('Invalid instance: ' + instance);
+    }
+}
+
+function _storeOptions(url, dbName) {
+    return {
+        couchDbUrl: url,
+        couchDbName: dbName
+    };
+}
