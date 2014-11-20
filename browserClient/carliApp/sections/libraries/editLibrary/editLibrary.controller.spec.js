3ba5d54c87036299bd56be442e072c7f980e222b
Lonesome Marty <marty@pixotech.com>
2014-11-19 20:39:07 -0600

    Clean up the edit Library Controller unit tests using more beforeEach code

--- browserClient/carliApp/sections/libraries/editLibrary/editLibrary.controller.spec.js
+++ browserClient/carliApp/sections/libraries/editLibrary/editLibrary.controller.spec.js
@@ -1,13 +1,13 @@
 
 describe('The Edit Library Controller', function(){
 
-    var mockDependenciesForNewLibrary, mockDependenciesForEditLibrary;
+    var newCtrl, editCtrl, mockDependenciesForNewLibrary, mockDependenciesForEditLibrary;
 
     beforeEach(function(){
         module('carli.sections.libraries.edit');
         module('carli.mockServices');
 
-        inject( function($q, mockLocationService, mockLibraryService ) {
+        inject( function($controller, $rootScope, $q, mockLocationService, mockLibraryService ) {
             mockDependenciesForNewLibrary = {
                 $location: mockLocationService,
                 $routeParams: {
@@ -23,63 +23,57 @@ describe('The Edit Library Controller', function(){
                 },
                 libraryService: mockLibraryService
             };
+
+            newCtrl  = $controller('editLibraryController', mockDependenciesForNewLibrary);
+            editCtrl = $controller('editLibraryController', mockDependenciesForEditLibrary);
+            $rootScope.$digest();
         });
     });
 
-    it('should export lists of options in the ViewModel', inject(function($controller){
-        var editCtrl = $controller('editLibraryController', mockDependenciesForNewLibrary);
-        expect( editCtrl.institutionYearsOptions ).to.be.an('Array');
-        expect( editCtrl.institutionTypeOptions ).to.be.an('Array');
-        expect( editCtrl.membershipLevelOptions ).to.be.an('Array');
-    }));
+    it('should export lists of options in the ViewModel', function(){
+        expect( newCtrl.institutionYearsOptions ).to.be.an('Array');
+        expect( newCtrl.institutionTypeOptions ).to.be.an('Array');
+        expect( newCtrl.membershipLevelOptions ).to.be.an('Array');
+    });
 
-    it('should have a default, editable library on the New Library screen', inject(function($controller){
-        var editCtrl = $controller('editLibraryController', mockDependenciesForNewLibrary);
-        expect( editCtrl.library.isActive ).to.equal(true);
-        expect( editCtrl.editable ).to.equal(true);
-        expect( editCtrl.newLibrary ).to.equal(true);
-    }));
+    it('should have a default, editable library on the New Library screen', function(){
+        expect( newCtrl.library.isActive ).to.equal(true);
+        expect( newCtrl.editable ).to.equal(true);
+        expect( newCtrl.newLibrary ).to.equal(true);
+    });
 
-    it('should call libraryService.create when saving a new Library', inject(function($controller){
-        var editCtrl = $controller('editLibraryController', mockDependenciesForNewLibrary);
+    it('should call libraryService.create when saving a new Library', function(){
         expect( mockDependenciesForNewLibrary.libraryService.createOrUpdate ).to.equal('neither');
-        editCtrl.saveLibrary();
+        newCtrl.saveLibrary();
         expect( mockDependenciesForNewLibrary.libraryService.createOrUpdate ).to.equal('create');
-    }));
+    });
 
-    it('should have a known, non-editable library on the Edit library screen', inject(function($controller, $rootScope){
-        var editCtrl = $controller('editLibraryController', mockDependenciesForEditLibrary);
-        $rootScope.$digest();
+    it('should have a known, non-editable library on the Edit library screen', function(){
         expect( editCtrl.library.name ).to.equal( 'Test Library');
         expect( editCtrl.editable ).to.equal(false);
         expect( editCtrl.newLibrary ).to.equal(false);
-    }));
+    });
 
-    it('should call libraryService.update when saving an existing library', inject(function($controller){
-        var editCtrl = $controller('editLibraryController', mockDependenciesForEditLibrary);
+    it('should call libraryService.update when saving an existing library', function(){
         expect( mockDependenciesForEditLibrary.libraryService.createOrUpdate ).to.equal('neither');
         editCtrl.saveLibrary();
         expect( mockDependenciesForEditLibrary.libraryService.createOrUpdate ).to.equal('update');
-    }));
+    });
 
-    it('should toggle the editable variable when calling toggleEditable()', inject(function($controller){
-        var editCtrl = $controller('editLibraryController', mockDependenciesForEditLibrary);
+    it('should toggle the editable variable when calling toggleEditable()', function(){
         expect( editCtrl.editable ).to.equal(false);
         editCtrl.toggleEditable();
         expect( editCtrl.editable ).to.equal(true);
-    }));
+    });
 
-    it('should be able to delete the first Director contact', inject(function($controller, $rootScope) {
-        var editCtrl = $controller('editLibraryController', mockDependenciesForEditLibrary);
-        $rootScope.$digest();
+    it('should be able to delete the first Director contact', function() {
         var contacts = editCtrl.library.contacts;
         var firstDirectorContact = findFirstDirectorContact(contacts);
         var initialLength = contacts.length;
 
         editCtrl.deleteContact(firstDirectorContact);
-        $rootScope.$digest();
         expect( contacts.length ).to.equal(initialLength - 1);
-    }));
+    });
 
 });
 
