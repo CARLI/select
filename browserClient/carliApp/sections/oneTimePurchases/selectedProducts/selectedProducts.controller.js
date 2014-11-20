cd7a2beaa8d81c98f9e77400c629726caa7fcca6
Bryan and Marty <marty@pixotech.com>
2014-11-19 19:36:37 -0600

    Papa-Bryan-Moto Refactor of the selectedProducts Controller.
    The order is:
    VM Variables
    VM Exported Functions
    activate()
    Function Definitions

--- browserClient/carliApp/sections/oneTimePurchases/selectedProducts/selectedProducts.controller.js
+++ browserClient/carliApp/sections/oneTimePurchases/selectedProducts/selectedProducts.controller.js
@@ -6,70 +6,16 @@ function selectedProductsController($routeParams, libraryService, productService
     vm.libraryId = $routeParams.libraryId;
     vm.productList = [];
     vm.filterState = 'all';
+    vm.selectedProducts = {};
 
-    vm.filter = function( value ){
-        return (
-            value.cycleType === 'One-Time Purchase' &&
-            value.isActive === true &&
-            (vm.filterState === 'all' || value.oneTimePurchase.libraryPurchaseData[vm.libraryId].datePurchased)
-        );
-    };
-
-    vm.setShowSelectedProducts = function () {
-        vm.filterState = 'selected';
-    };
-
-    vm.setShowAllProducts = function() {
-        vm.filterState = 'all';
-    };
-
-    vm.purchaseProduct = function(product) {
-        product.oneTimePurchase.libraryPurchaseData[vm.libraryId].datePurchased = new Date().toJSON().slice(0,10);
-        productService.update(product).then(function(){
-            //TODO: add notification of success (inline?)
-            //TODO: add audit trail entry (in service)
-        })
-        .catch(function(){
-            //TODO: notification of failure (inline?)
-            product.oneTimePurchase.libraryPurchaseData[vm.libraryId].datePurchased = null;
-        });
-    };
-
-    vm.cancelPurchase = function(product) {
-        var oldDate = product.oneTimePurchase.libraryPurchaseData[vm.libraryId].datePurchased;
-
-        product.oneTimePurchase.libraryPurchaseData[vm.libraryId].datePurchased = null;
-        productService.update(product).then(function(){
-            //TODO: add notification of success (inline?)
-            //TODO: add audit trail entry (in service)
-        })
-        .catch(function(){
-            //TODO: notification of failure (inline?)
-            product.oneTimePurchase.libraryPurchaseData[vm.libraryId].datePurchased = oldDate;
-        });
-    };
-
-    vm.computeTotalPurchasesAmount = function() {
-        var totalAmount = 0;
-        var product;
-
-        for (i=0; i<vm.productList.length; i++) {
-            product = vm.productList[i];
-            if (vm.filter(product) &&
-                product.oneTimePurchase.libraryPurchaseData[vm.libraryId].datePurchased) {
-                totalAmount += product.oneTimePurchase.libraryPurchaseData[vm.libraryId].price;
-            }
-        }
-        return totalAmount;
-    };
-
-    vm.invoiceProducts = function() {
-        alert("Invoice Products:" + JSON.stringify(vm.selectedProducts));
-    };
-
-    vm.reportProducts = function() {
-        alert("Report Products:" + JSON.stringify(vm.selectedProducts));
-    };
+    vm.filter = filter;
+    vm.setShowSelectedProducts = setShowSelectedProducts;
+    vm.setShowAllProducts = setShowAllProducts;
+    vm.purchaseProduct = purchaseProduct;
+    vm.cancelPurchase = cancelPurchase;
+    vm.computeTotalPurchasesAmount = computeTotalPurchasesAmount;
+    vm.invoiceProducts = invoiceProducts;
+    vm.reportProducts = reportProducts;
 
     activate();
 
@@ -81,5 +27,69 @@ function selectedProductsController($routeParams, libraryService, productService
         productService.listOneTimePurchaseProducts().then( function(productList){
             vm.productList = productList;
         });
+    }
+
+    function filter( value ){
+        return (
+            value.cycleType === 'One-Time Purchase' &&
+            value.isActive === true &&
+            (vm.filterState === 'all' || value.oneTimePurchase.libraryPurchaseData[vm.libraryId].datePurchased)
+        );
+    }
+
+    function setShowSelectedProducts() {
+        vm.filterState = 'selected';
+    }
+
+    function setShowAllProducts() {
+        vm.filterState = 'all';
+    }
+
+    function purchaseProduct(product) {
+        product.oneTimePurchase.libraryPurchaseData[vm.libraryId].datePurchased = new Date().toJSON().slice(0,10);
+        productService.update(product).then(function(){
+            //TODO: add notification of success (inline?)
+            //TODO: add audit trail entry (in service)
+        })
+        .catch(function(){
+            //TODO: notification of failure (inline?)
+            product.oneTimePurchase.libraryPurchaseData[vm.libraryId].datePurchased = null;
+        });
+    }
+
+    function cancelPurchase(product) {
+        var oldDate = product.oneTimePurchase.libraryPurchaseData[vm.libraryId].datePurchased;
+
+        product.oneTimePurchase.libraryPurchaseData[vm.libraryId].datePurchased = null;
+        productService.update(product).then(function(){
+            //TODO: add notification of success (inline?)
+            //TODO: add audit trail entry (in service)
+        })
+        .catch(function(){
+            //TODO: notification of failure (inline?)
+            product.oneTimePurchase.libraryPurchaseData[vm.libraryId].datePurchased = oldDate;
+        });
+    }
+
+    function computeTotalPurchasesAmount() {
+        var totalAmount = 0;
+        var product;
+
+        for (var i=0; i<vm.productList.length; i++) {
+            product = vm.productList[i];
+            if (vm.filter(product) &&
+                product.oneTimePurchase.libraryPurchaseData[vm.libraryId].datePurchased) {
+                totalAmount += product.oneTimePurchase.libraryPurchaseData[vm.libraryId].price;
+            }
+        }
+        return totalAmount;
+    }
+
+    function invoiceProducts() {
+        alert("Invoice Products:" + JSON.stringify(vm.selectedProducts));
+    }
+
+    function reportProducts() {
+        alert("Report Products:" + JSON.stringify(vm.selectedProducts));
     }
 }
