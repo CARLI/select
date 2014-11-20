27aea37877c7068bf56a48c60a38d6276d17b967
Lonesome Marty <marty@pixotech.com>
2014-11-19 19:47:49 -0600

    CARLI-148 Unit test the VM exports of the One-Time Purchase Select Products screen.

--- browserClient/carliApp/sections/oneTimePurchases/selectedProducts/selectedProducts.controller.spec.js
+++ browserClient/carliApp/sections/oneTimePurchases/selectedProducts/selectedProducts.controller.spec.js
@@ -4,7 +4,7 @@ describe('The One-time Purchases Selected-Products Controller', function(){
     var mockDependenciesForOneTimePurchase;
 
     beforeEach(function(){
-        module('carli.sections.oneTimePurchases');
+        module('carli.sections.oneTimePurchases.selectedProducts');
         module('carli.mockServices');
 
         inject(function ($q, mockLibraryService, mockProductService) {
@@ -18,11 +18,28 @@ describe('The One-time Purchases Selected-Products Controller', function(){
         });
     });
 
-    it('should expose a list of Libraries and List Columns', inject(function($controller, $rootScope){
-        var vm = $controller('oneTimePurchasesController', mockDependenciesForOneTimePurchase);
+    it('should expose VM variables', inject(function($controller, $rootScope){
+        var vm = $controller('selectedProductsController', mockDependenciesForOneTimePurchase);
         $rootScope.$digest();
-        expect(vm.libraryList).to.be.an('Array');
-        expect(vm.libraryListColumns).to.be.an('Array');
+
+        expect(vm.libraryId).to.be.a('String');
+        expect(vm.productList).to.be.an('Array');
+        expect(vm.filterState).to.equal('all');
+        expect(vm.selectedProducts).to.be.an('Object');
+    }));
+
+    it('should expose VM functions', inject(function($controller, $rootScope){
+        var vm = $controller('selectedProductsController', mockDependenciesForOneTimePurchase);
+        $rootScope.$digest();
+
+        expect(vm.filter).to.be.a('Function');
+        expect(vm.setShowSelectedProducts).to.be.a('Function');
+        expect(vm.setShowAllProducts).to.be.a('Function');
+        expect(vm.purchaseProduct).to.be.a('Function');
+        expect(vm.cancelPurchase).to.be.a('Function');
+        expect(vm.computeTotalPurchasesAmount).to.be.a('Function');
+        expect(vm.invoiceProducts).to.be.a('Function');
+        expect(vm.reportProducts).to.be.a('Function');
     }));
 
 });
