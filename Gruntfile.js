ed180ece2b402815e974bb1e91bfd5777d6d6965
Han Solo <bryan@pixotech.com>
2014-11-24 10:29:15 -0600

    Top-level install-deps now runs it's own npm install

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,6 +8,7 @@ module.exports = function(grunt) {
     });
 
     grunt.registerTask('npm-install', function() {
+        grunt.task.run(['subdir-exec:.:npm install']);
         grunt.task.run(['subdir-exec:browserClient:npm install']);
         grunt.task.run(['subdir-exec:browserClient/e2e:npm install']);
         grunt.task.run(['subdir-exec:CARLI:npm install']);
