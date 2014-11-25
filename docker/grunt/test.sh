bb35e8eb43a1dcfd1a3535daffea9790c3fe166f
Han Solo <bryan@pixotech.com>
2014-11-25 15:55:58 -0600

    Our tests don't currently support testing in more than 1 browser

--- docker/grunt/test.sh
+++ docker/grunt/test.sh
@@ -4,4 +4,4 @@ echo "Configuring CARLI for test instance"
 cd /carli-select/CARLI && grunt generate-couch-config:test
 cd /carli-select && grunt deploy-db
 cd /carli-select &&
-    CARLI_TEST_ENABLE_CHROME="yes" CARLI_TEST_ENABLE_FIREFOX="yes" CARLI_TEST_ENABLE_SHARDING="no" grunt test:jenkins
+    CARLI_TEST_ENABLE_CHROME="no" CARLI_TEST_ENABLE_FIREFOX="yes" CARLI_TEST_ENABLE_SHARDING="no" grunt test:jenkins
