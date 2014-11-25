f3796531d568a4246217e149d2e9ef725d6e42c3
Han Solo <bryan@pixotech.com>
2014-11-25 15:50:29 -0600

    Disable sharding tests on jenkins

--- docker/grunt/test.sh
+++ docker/grunt/test.sh
@@ -3,4 +3,5 @@
 echo "Configuring CARLI for test instance"
 cd /carli-select/CARLI && grunt generate-couch-config:test
 cd /carli-select && grunt deploy-db
-cd /carli-select && grunt test:jenkins
+cd /carli-select &&
+    CARLI_TEST_ENABLE_CHROME="yes" CARLI_TEST_ENABLE_FIREFOX="yes" CARLI_TEST_ENABLE_SHARDING="no" grunt test:jenkins
