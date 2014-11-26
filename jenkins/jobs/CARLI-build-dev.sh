b06352084484472f0690e4fc721f23d518203ce7
Han Solo <bryan@pixotech.com>
2014-11-26 14:14:10 -0600

    unnecessary quotes && stale comment removal

--- jenkins/jobs/CARLI-build-dev.sh
+++ jenkins/jobs/CARLI-build-dev.sh
@@ -8,17 +8,12 @@ SCRIPTS=./jenkins/scripts
 $SCRIPTS/add-build-number.sh dev &&
 $SCRIPTS/build-container.sh couchdb &&
 $SCRIPTS/build-container.sh grunt &&
-$SCRIPTS/tag-container.sh couchdb "latest" &&
-$SCRIPTS/tag-container.sh grunt "latest" &&
+$SCRIPTS/tag-container.sh couchdb latest &&
+$SCRIPTS/tag-container.sh grunt latest &&
 $SCRIPTS/run-db-container.sh dev latest 9091 &&
 $SCRIPTS/run-test-container.sh dev latest &&
-$SCRIPTS/tag-container.sh couchdb "last-good" &&
-$SCRIPTS/tag-container.sh grunt "last-good"
+$SCRIPTS/tag-container.sh couchdb last-good &&
+$SCRIPTS/tag-container.sh grunt last-good
 
 $SCRIPTS/run-serve-container.sh dev latest 9090
- 
-# It isn't necessary to export the image right now, because we 
-# are serving both carli.dev and carli.qa directly from the same
-# machine that does the builds.
 
-# $SCRIPTS/export-container.sh grunt
