6e8593cb8978fb8c9026f295ed4b465823fc96ba
Han Solo <bryan@pixotech.com>
2014-12-02 11:56:45 -0600

    CARLI-127 Licenses: fix submit button on edit page

--- browserClient/carliApp/sections/licenses/editLicense/editLicense.controller.js
+++ browserClient/carliApp/sections/licenses/editLicense/editLicense.controller.js
@@ -1,12 +1,10 @@
 angular.module('carli.sections.licenses.edit')
     .controller('editLicensePageController', editLicensePageController);
 
-function editLicensePageController( $routeParams ) {
+function editLicensePageController( $routeParams, $location ) {
     var vm = this;
     vm.licenseId = $routeParams.id;
-    console.log('hello');
-    console.log(vm.licenseId);
-    vm.afterVendorSubmit = routeToLicenseList;
+    vm.afterLicenseSubmit = routeToLicenseList;
 
     function routeToLicenseList() {
         $location.path('/license');
