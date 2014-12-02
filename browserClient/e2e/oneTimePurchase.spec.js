0f86e818408f1c231ddb5a12ec200aad9bf46857
Dan Solo <dans@pixotech.com>
2014-12-02 15:01:12 -0600

    CARLI-169 added search test and a broken attempt to test the select product page for one-time purchases

--- browserClient/e2e/oneTimePurchase.spec.js
+++ browserClient/e2e/oneTimePurchase.spec.js
@@ -1,12 +1,15 @@
 var config = require('./utils/baseConfig'),
     dataLoader = require('./utils/fixtureDataLoader'),
-    testData = require('./oneTimePurchase.fixtures');
+    testData = require('./oneTimePurchase.fixtures'),
+    macro = require('./utils/protractorMacros');
 
 config.getDefaultAppPage();
 
 dataLoader.createLibrary(testData.activeLibrary1);
 dataLoader.createLibrary(testData.inactiveLibrary2);
 dataLoader.createLibrary(testData.activeLibrary3);
+dataLoader.createVendor(testData.activeVendor1);
+dataLoader.createProduct(testData.activePurchasedProduct1);
 
 var NAME_COLUMN_INDEX=0;
 var FTE_COLUMN_INDEX=1;
@@ -50,6 +53,28 @@ describe('The One-Time Purchases screen', function () {
             });
     });
 
+    it("should search by name only, (not include other field values)", function () {
+
+        // activeLibrary3 has "Other" in a non-name field -- should not appear in list if searching on Other
+        inputSearch = macro.elementByModel("filterText");
+        macro.setInputValue(inputSearch, "Other");
+        element.all(by.repeater('entity in values'))
+            .then( function( libraryList ) {
+                for (i=0; i<libraryList.length; i++) {
+                    expect(libraryList[i].getText()).toNotContain(testData.activeLibrary3.name);
+                }
+            });
+
+        // Search using UUID of library 1 -- should list only that library
+        macro.setInputValue(inputSearch, testData.activeLibrary1.name);
+        element.all(by.repeater('entity in values'))
+            .then( function( libraryList ) {
+                expect(libraryList[0].getText()).toContain(testData.activeLibrary1.name);
+            });
+
+
+    });
+
     it("should go to the One-Time Purchase select product page when the View button is clicked", function () {
         element.all(by.repeater('entity in values'))
             .filter( function(el, index) {
@@ -71,18 +96,48 @@ describe('The One-Time Purchases screen', function () {
 /*
 describe('The One-Time Purchases Select Product screen', function () {
 
+    console.log('activePurchasedProduct1=' + JSON.stringify(testData.activePurchasedProduct1));
+
     it('should only contain products of type cycleType One-Time Purchase', function(){
         element.all(by.repeater('product in vm.productList'))
+            .filter( function(el, index) {
+                return el.getText().then(function(text){
+                    return (text.search(testData.activePurchasedProduct1.name) > -1);
+                });
+            })
             .then( function( productList ) {
-                console.log("productList=" + JSON.stringify(productList));
-                for (var i=0; i<productList.length; i++) {
-                    console.log("product=" + JSON.stringify(productList[i]));
-                    console.log("product.cycleType=" + productList[i].cycleType);
-                    expect( productList[i].cycleType ).toBe("One-Time Purchase");
-                }
-            });
 
+
+                //productList[0].element(by.id('product-name')).getText().then(function(text) {
+                //    console.log("prod name=" + text);
+                //});
+
+
+                // This attempts to find a field in the list by name, but doesn't work because ids need to be unique per DOM
+                expect( productList[0].element(by.id('product-name')).getText()).toBe(testData.activePurchasedProduct1.name);
+
+
+                // This attempts to find the columns in the product list and verify each field contains what the fixture
+                // data says it should.  I think name currently works, but the others don't.
+                productList[0].all(by.className('column'))
+                    .then (function(colList) {
+          console.log("colListLength=" + colList.length);
+                    var lib1Name =  testData.activeLibrary1.name;
+          console.log("lib1Name=" + lib1Name);
+          console.log("column[0]=" + colList[0].getText());
+
+           console.log("calling expect, prod name = " + testData.activePurchasedProduct1.name);
+                    expect( colList[0].getText()).toBe(testData.activePurchasedProduct1.name);
+           console.log("calling expect, vendor name = " + testData.activePurchasedProduct1.vendor.name);
+                    expect( colList[1].getText()).toBe(testData.activePurchasedProduct1.vendor.name);
+           console.log("cost=" + testData.activePurchasedProduct1.oneTimePurchase.libraryPurchaseData[lib1Name].price);
+                    expect( colList[2].getText()).toBe(testData.activePurchasedProduct1.oneTimePurchase.libraryPurchaseData[lib1Name].price);
+                });
+            });
     });
 
+
+
 });
+
     */
