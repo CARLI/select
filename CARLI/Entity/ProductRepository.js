b3b7733ed554c3c90adc503b1b4164d5bc95b450
Marty and Bryan <bryan@pixotech.com>
2014-12-04 16:17:03 -0600

    CARLI-212 Added custom repository function for listing available One-Time Purchase Products.
    Included mocha tests for Products Entity. And Moment.js

--- CARLI/Entity/ProductRepository.js
+++ CARLI/Entity/ProductRepository.js
@@ -1,8 +1,37 @@
 var Entity = require('../Entity')
+  , moment = require('moment')
+  , Q = require('q')
     ;
 
 var ProductRepository = Entity('Product');
 
-ProductRepository.publicFunc = function(){};
+ProductRepository.listAvailableOneTimePurchaseProducts = function(){
+    var deferred = Q.defer();
+
+    ProductRepository.list()
+        .then(function (allProducts) {
+            var p = allProducts
+                .filter(isOneTimePurchaseProduct)
+                .filter(isActive)
+                .filter(isAvailableToday);
+            deferred.resolve(p);
+        })
+        .catch(function (err) {
+            deferred.reject(err);
+        });
+    return deferred.promise;
+};
+
+function isOneTimePurchaseProduct( product ){
+    return product.cycleType === 'One-Time Purchase';
+}
+function isActive( product ){
+    return product.isActive === true;
+}
+function isAvailableToday( product ){
+    var throughDate = moment(product.oneTimePurchase.availableForPurchaseThrough);
+    var lastMidnight = moment().startOf('day');
+    return throughDate.isAfter(lastMidnight);
+}
 
 module.exports = ProductRepository;
---
+++ CARLI/test/Product.spec.js
@@ -0,0 +1,84 @@
+var chai   = require( 'chai' )
+    , expect = chai.expect
+    , uuid   = require( 'node-uuid' )
+    , chaiAsPromised = require( 'chai-as-promised' )
+    , moment = require('moment')
+    , test = require( './Entity/EntityInterface.spec' )
+    ;
+
+chai.use( chaiAsPromised );
+
+var yesterday = moment().subtract(1, 'week');
+var tomorrow = moment().add(1, 'week');
+
+
+function validProductData() {
+    return {
+        type: 'Product',
+        name: 'foo',
+        isActive: true,
+        vendor: 'fake-vendor-id'
+    };
+}
+function invalidProductData() {
+    return {
+        type: 'Product'
+    };
+}
+
+test.run('Product', validProductData, invalidProductData);
+
+function availableOneTimePurchaseProduct() {
+    var product = validProductData();
+    product.id = uuid.v4();
+    product.cycleType = 'One-Time Purchase';
+    product.oneTimePurchase = { availableForPurchaseThrough: tomorrow.toISOString() };
+    return product;
+}
+function unavailableOneTimePurchaseProduct() {
+    var product = validProductData();
+    product.id = uuid.v4();
+    product.cycleType = 'One-Time Purchase';
+    product.oneTimePurchase = { availableForPurchaseThrough: yesterday.toISOString() };
+    return product;
+}
+
+describe('ProductRepository.listOneTimePurchaseProducts()', function() {
+    var ProductRepository = require('../Entity/ProductRepository' );
+
+    it('should list a product that is available through tomorrow', function () {
+        var availableProduct = availableOneTimePurchaseProduct();
+
+        return ProductRepository.create(availableProduct)
+            .then(function() {
+                return ProductRepository.listAvailableOneTimePurchaseProducts()
+            },
+            function catchCreate(err) {
+                console.log('Create failed: ' + err);
+            })
+            .then(function(otpList) {
+                return expect(otpList).to.deep.include.members([availableProduct]);
+            },
+            function catchList(err) {
+                console.log('listAvailableOneTimePurchaseProducts failed: ' + err);
+            })
+    });
+
+    it('should not list a product that was available through yesterday', function () {
+        var unavailableProduct = unavailableOneTimePurchaseProduct();
+
+        return ProductRepository.create(unavailableProduct)
+            .then(function () {
+                return ProductRepository.listAvailableOneTimePurchaseProducts()
+            },
+            function catchCreate(err) {
+                console.log('Create failed: ' + err);
+            })
+            .then(function (otpList) {
+                return expect(otpList).to.not.deep.include.members([unavailableProduct]);
+            },
+            function catchList(err) {
+                console.log('listAvailableOneTimePurchaseProducts failed: ' + err);
+            })
+    });
+});
