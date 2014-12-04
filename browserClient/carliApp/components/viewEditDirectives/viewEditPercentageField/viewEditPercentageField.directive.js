c62d322e3a2fa7bdcdc781080221fab0ff69d21b
Marty and Dan <dans@pixotech.com>
2014-12-04 13:34:52 -0600

    CARLI-211 Add a viewEditPercentageField component

---
+++ browserClient/carliApp/components/viewEditDirectives/viewEditPercentageField/viewEditPercentageField.directive.js
@@ -0,0 +1,9 @@
+angular.module('carli.viewEditDirectives.viewEditPercentageField')
+    .directive('viewEditPercentageField', function() {
+        return {
+            restrict: 'E',
+            templateUrl: '/carliApp/components/viewEditDirectives/viewEditPercentageField/viewEditPercentageField.html',
+            scope: { ngModel: '=', editMode: '=' },
+            transclude: true
+        };
+    });
