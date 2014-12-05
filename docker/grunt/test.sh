5ad5a8407d389d8583927499da3bc9b125f216e3
Han Solo <bryan@pixotech.com>
2014-12-05 16:39:04 -0600

    Set jsenv to node for config generation

--- docker/grunt/test.sh
+++ docker/grunt/test.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 echo "Configuring CARLI for test instance"
-cd /carli-select/CARLI && grunt generate-config:test
+
+cd /carli-select/CARLI && grunt jsenv:node && grunt generate-config:test
 cd /carli-select && grunt deploy-db
 cd /carli-select &&
     CARLI_TEST_ENABLE_CHROME="no" CARLI_TEST_ENABLE_FIREFOX="yes" CARLI_TEST_ENABLE_SHARDING="no" grunt test:jenkins
