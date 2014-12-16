7521976d332328e382ade6fcbbcc7f8899fb8e95
Marty and Dan <dans@pixotech.com>
2014-12-16 10:05:05 -0600

    CARLI-216 Remove entity transformation logic from license service (replaced by logic in LicenseRepository)

--- browserClient/carliApp/components/licenseService/licenseService.service.js
+++ browserClient/carliApp/components/licenseService/licenseService.service.js
@@ -3,60 +3,12 @@ angular.module('carli.licenseService')
 
 function licenseService( CarliModules, $q, entityBaseService, vendorService ) {
 
-    var references = { vendor: vendorService };
     var licenseModule = CarliModules.License;
-    var licenseStore = CarliModules.Store( CarliModules[CarliModules.config.store]( CarliModules.config.storeOptions ) );
-    licenseModule.setStore( licenseStore );
 
     return {
-        list:   listLicenses,
-        create: createLicense,
-        update: updateLicense,
-        load:   loadLicense
+        list:   function() { return $q.when( licenseModule.list() ); },
+        create: function() { return $q.when( licenseModule.create.apply(this, arguments) ); },
+        update: function() { return $q.when( licenseModule.update.apply(this, arguments) ); },
+        load:   function() { return $q.when( licenseModule.load.apply(this, arguments) ); },
     };
-
-    function listLicenses() {
-        return entityBaseService.expandReferencesToObjects($q.when(licenseModule.list()), references);
-    }
-
-    function createLicense(license) {
-        entityBaseService.transformObjectsToReferences(license, references);
-        return $q.when( licenseModule.create(license) );
-    }
-
-    function updateLicense(license) {
-        var deferred = $q.defer();
-        var savedObjects = entityBaseService.saveReferences(license, references);
-        entityBaseService.transformObjectsToReferences(license, references);
-
-        licenseModule.update(license)
-            .then(function (license) {
-               deferred.resolve(license);
-            })
-            .catch(function (err) {
-                deferred.reject(err);
-            })
-            .finally(function () {
-                entityBaseService.restoreReferences(license, savedObjects, references);
-            });
-
-        return deferred.promise;
-    }
-
-    function loadLicense(id) {
-        var deferred = $q.defer();
-
-        licenseModule.load(id)
-            .then(function (license) {
-                entityBaseService.fetchAndTransformObjectsFromReferences(license, references)
-                    .then(function (license) {
-                        deferred.resolve(license);
-                    });
-            })
-            .catch(function (err) {
-                deferred.reject(err);
-            });
-
-        return deferred.promise;
-    }
 }
