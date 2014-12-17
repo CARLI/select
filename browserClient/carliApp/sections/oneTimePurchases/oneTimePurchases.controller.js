c6d49307742199cbeea547fd6b29b53645f581f7
Lonesome Marty <marty@pixotech.com>
2014-12-17 16:05:56 -0600

    CARI-170 Just kidding, I forgot the One-Time-Purchase Library list.

--- browserClient/carliApp/sections/oneTimePurchases/oneTimePurchases.controller.js
+++ browserClient/carliApp/sections/oneTimePurchases/oneTimePurchases.controller.js
@@ -7,20 +7,24 @@ function oneTimePurchasesController( $sce, libraryService ){
     vm.libraryListColumns = [
         {
             label: "Library",
+            orderByProperty: 'name',
             contentFunction: function(library) {
                 return $sce.trustAsHtml('<a href="oneTimePurchases/' + library.id + '">' + library.name + '</a>');
             }
         },
         {
             label: "FTE",
+            orderByProperty: 'fte',
             contentFunction: function(library) { return library.fte; }
         },
         {
             label: "Type",
+            orderByProperty: 'institutionType',
             contentFunction: function(library) { return library.institutionType; }
         },
         {
             label: "",
+            orderByProperty: false,
             contentFunction: function(library) {
                 return $sce.trustAsHtml('<a class="carli-button list-button" href="oneTimePurchases/' + library.id + '">View <i class="fa fa-chevron-circle-right"></i></a>');
             }
