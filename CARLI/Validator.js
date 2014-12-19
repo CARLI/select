321570818729abe98cc9a283926497f280dc323d
Lonesome Marty <marty@pixotech.com>
2014-12-19 16:20:45 -0600

    CARLI-115 Added a getEnumValuesFor method to the Validator

--- CARLI/Validator.js
+++ CARLI/Validator.js
@@ -6,6 +6,7 @@ var schemas = {
     Contact: require('../schemas/Contact.json'),
     CycleType: require('../schemas/CycleType.json'),
     Date: require('../schemas/Date.json'),
+    InstitutionType: require('../schemas/InstitutionType.json'),
     Library: require('../schemas/Library.json'),
     License: require('../schemas/License.json'),
     OneTimePurchase: require('../schemas/OneTimePurchase.json'),
@@ -52,7 +53,29 @@ function validate(data) {
     return deferred.promise;
 }
 
+function getEnumValuesFor( type, property ){
+    if (type === undefined){
+        throw new Error('Type Required');
+    }
+
+    if (!schemas[type]){
+        throw new Error('Unknown Type');
+    }
+
+    var schema = schemas[type];
+
+    if ( schema.enum ){
+        return schemas[type]['enum'];
+    }
+    else if (property === undefined){
+        throw new Error('Property Required');
+    }
+
+    return schema['properties'][property]['enum'];
+}
+
 module.exports = {
     list: listValidTypes,
-    validate: validate
+    validate: validate,
+    getEnumValuesFor: getEnumValuesFor
 };
