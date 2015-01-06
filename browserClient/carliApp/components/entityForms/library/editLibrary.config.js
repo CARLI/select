019f2dad5200155d9b1bf460061b83fe3fcd94e9
Lonesome Marty <marty@pixotech.com>
2015-01-06 14:33:26 -0600

    CARLI-77 Add the warn-if-unsaved module to the edit forms

--- browserClient/carliApp/components/entityForms/library/editLibrary.config.js
+++ browserClient/carliApp/components/entityForms/library/editLibrary.config.js
@@ -2,5 +2,6 @@ angular.module('carli.entityForms.library',[
     'carli.alerts',
     'carli.entityBaseService',
     'carli.libraryService',
-    'carli.viewEditDirectives'
+    'carli.viewEditDirectives',
+    'carli.warnIfUnsaved'
 ]);
--- browserClient/carliApp/components/entityForms/product/editProduct.config.js
+++ browserClient/carliApp/components/entityForms/product/editProduct.config.js
@@ -1,9 +1,9 @@
 angular.module('carli.entityForms.product',[
     'ui.bootstrap',
-
     'carli.alerts',
     'carli.libraryService',
     'carli.licenseService',
     'carli.productService',
-    'carli.viewEditDirectives'
+    'carli.viewEditDirectives',
+    'carli.warnIfUnsaved'
 ]);
