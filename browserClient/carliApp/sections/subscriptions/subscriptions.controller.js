68167e29d1a2914326f141d5321f78743e430506
Bryan and Marty <marty@pixotech.com>
2015-01-07 14:37:27 -0600

    CARLI-284 Add subscriptions section

--- browserClient/carliApp/sections/subscriptions/subscriptions.controller.js
+++ browserClient/carliApp/sections/subscriptions/subscriptions.controller.js
@@ -3,4 +3,17 @@ angular.module('carli.sections.subscriptions')
 
 function subscriptionsController(){
     var vm = this;
+
+    activate();
+    function activate() {
+        addDummyData();
+    }
+
+    function addDummyData() {
+        vm.cycles = [
+            { name: 'Fiscal Year 2015', status: 'Edit Product List' },
+            { name: 'Calendar Year 2015', status: 'Products Available to Library Patrons' },
+            { name: 'Fiscal Year 2014', status: 'Products Available to Library Patrons' }
+        ];
+    }
 }
