40237f65951f0f95e7d29533f7621809c99549ef
Bryan and Marty <marty@pixotech.com>
2015-01-28 16:48:06 -0600

    Grunt tasks for Jenkins

--- docker/grunt/serve.sh
+++ docker/grunt/serve.sh
@@ -6,6 +6,9 @@ if [ -z "$instance" ]; then
 fi
 
 echo "Configuring CARLI for $instance instance"
+
+cd /carli-select && grunt jsenv:node
 cd /carli-select/jenkins && grunt generate-config:$instance
-cd /carli-select && grunt jsenv:node && grunt deploy-db && grunt fixture-data
-cd /carli-select/browserClient && grunt serve:headless
+cd /carli-select && grunt deploy-db && grunt fixture-data
+
+cd /carli-select && grunt serve:headless
--- docker/grunt/test.sh
+++ docker/grunt/test.sh
@@ -2,7 +2,9 @@
 
 echo "Configuring CARLI for test instance"
 
-cd /carli-select/jenkins && grunt jsenv:node && grunt generate-config:test
+cd /carli-select && grunt jsenv:node
+cd /carli-select/jenkins && grunt generate-config:test
 cd /carli-select && grunt deploy-db
+
 cd /carli-select &&
     CARLI_TEST_ENABLE_CHROME="no" CARLI_TEST_ENABLE_FIREFOX="yes" CARLI_TEST_ENABLE_SHARDING="no" grunt test:jenkins
