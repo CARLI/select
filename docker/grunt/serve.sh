c0781c35eaea2cdeb0084de523d7445d0fc5c739
Bryan and Marty <bryan@pixotech.com>
2015-02-02 11:20:31 -0600

    Do not blow away & redeploy data, now that it persists across rebuilds

--- docker/grunt/serve.sh
+++ docker/grunt/serve.sh
@@ -9,6 +9,8 @@ echo "Configuring CARLI for $instance instance"
 
 cd /carli-select && grunt jsenv:node
 cd /carli-select/jenkins && grunt generate-config:$instance
-cd /carli-select && grunt deploy-db && grunt fixture-data
+
+# Test data now persists across rebuilds, so don't blow it away every time.
+# cd /carli-select && grunt deploy-db && grunt fixture-data
 
 cd /carli-select && grunt serve:headless
--- jenkins/scripts/run-data-container.sh
+++ jenkins/scripts/run-data-container.sh
@@ -3,6 +3,14 @@
 instance=$1
 tag=$2
 
-sudo docker rm -f carli-data-$instance 2> /dev/null
-sudo docker run --name "carli-data-$instance" carli-couchdb:$tag /bin/echo Couch Data-only Container
+sudo docker inspect carli-data-$instance 2>&1 > /dev/null
+data_container_status=$?
 
+# Do not recreate the data container if it already exists.
+# The point of this container is persist the data storage.
+if [ $data_container_status -eq 1 ]; then
+    sudo docker run \
+        --name "carli-data-$instance" \
+        carli-couchdb:$tag \
+        /bin/echo Couch Data-only Container
+fi
