ced1c8c1954855add46abe2bb2f654ad025c06e1
Bryan and Marty <marty@pixotech.com>
2015-02-04 16:43:33 -0600

    Fix the offering service display options method

--- browserClient/carliApp/components/offeringService/offeringService.service.js
+++ browserClient/carliApp/components/offeringService/offeringService.service.js
@@ -35,6 +35,7 @@ function offeringService( CarliModules, $q, cycleService ) {
                     value: value
                 });
             });
+            return options;
         }
     };
 }
