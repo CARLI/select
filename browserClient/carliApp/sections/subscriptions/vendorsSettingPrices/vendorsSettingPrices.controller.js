3fe0c993416eb12b01ecb614164190e5cd1a4cc9
Bryan and Marty <marty@pixotech.com>
2015-02-04 16:46:09 -0600

    CARLI-289 Load libraries once, and add more inputs to the offerings rows

--- browserClient/carliApp/sections/subscriptions/vendorsSettingPrices/vendorsSettingPrices.controller.js
+++ browserClient/carliApp/sections/subscriptions/vendorsSettingPrices/vendorsSettingPrices.controller.js
@@ -1,18 +1,27 @@
 angular.module('carli.sections.subscriptions.vendorsSettingPrices')
     .controller('vendorsSettingPricesController', vendorsSettingPricesController);
 
-function vendorsSettingPricesController( $scope, alertService, vendorService, offeringService, productService ) {
+function vendorsSettingPricesController( $scope, alertService, cycleService, libraryService, vendorService, offeringService, productService ) {
     var vm = this;
     vm.closeVendorPricing = closeVendorPricing;
     vm.offeringDisplayOptions = offeringService.getOfferingDisplayOptions();
     vm.loadProductsForVendor = loadProductsForVendor;
+    vm.setOfferingEditable = setOfferingEditable;
+    vm.saveOffering = saveOffering;
     vm.vendors = [];
+    vm.isEditing = {};
+    vm.cycle = {};
 
     activate();
 
     function activate () {
         initSortable();
-        loadVendors();
+
+        vm.cycle = cycleService.getCurrentCycle();
+
+        libraryService.list()
+            .then(initLibraryList)
+            .then(loadVendors);
     }
 
     function initSortable() {
@@ -34,6 +43,14 @@ function vendorsSettingPricesController( $scope, alertService, vendorService, of
         };
     }
 
+    function initLibraryList( libraryList ){
+        vm.libraryMap = {};
+
+        libraryList.forEach(function(library){
+            vm.libraryMap[library.id] = library;
+        });
+    }
+
     function loadVendors() {
         vendorService.list().then(function (vendors) {
             vm.vendors = vendors;
@@ -50,13 +67,42 @@ function vendorsSettingPricesController( $scope, alertService, vendorService, of
             angular.forEach(products, function (product) {
                 offeringService.listOfferingsForProductId(product.id).then(function(offerings) {
                     product.offerings = offerings;
+
+                    offerings.forEach(function(offering){
+                        /*XXX*/offering.flagged = Math.random() > 0.5;
+                        offering.library = vm.libraryMap[offering.library];
+                        if (!offering.libraryComments) {
+                            offering.libraryComments = offering.product.comments;
+                        }
+                    });
                 });
             });
         });
+    }
+
+    function setOfferingEditable( offering ){
+        vm.isEditing[offering.id] = true;
+    }
+
+    function saveOffering( offering ) {
+        if (offering.libraryComments === offering.product.comments) {
+            delete offering.libraryComments;
+        }
+        /*
+        offeringService.update(offering).then(function(result){
+            //alert(?)
+            console.log(result);
+            vm.isEditing[offering.id] = false;
+        }).catch(function(err) {
+            console.log('failed', err);
+        });
+        */
+        /*XXX*/vm.isEditing[offering.id] = false;
     }
 
     function closeVendorPricing(){
         $scope.cycle.returnToPreviousStep();
         //TODO: persist the cycle and add a success alert when it's saved
     }
+
 }
