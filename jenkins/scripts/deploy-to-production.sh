fe7975a43b4e182c173fbea924061826b598c73e
Bryan and Marty <bryan@pixotech.com>
2015-02-09 09:27:44 -0600

    Add data persistance container to prod deployment

--- jenkins/scripts/deploy-to-production.sh
+++ jenkins/scripts/deploy-to-production.sh
@@ -8,5 +8,6 @@ rm /home/jenkins/carli-grunt.tgz
 cat /home/jenkins/carli-couchdb.tgz | gunzip | sudo docker load
 rm /home/jenkins/carli-couchdb.tgz
 
+/home/jenkins/scripts/run-data-container.sh prod last-good
 /home/jenkins/scripts/run-db-container.sh prod last-good 5984
 /home/jenkins/scripts/run-serve-container.sh prod last-good 80
