0a4d1d8ba7f0722527d5c44a41f737f28f977822
Bryan and Marty <marty@pixotech.com>
2015-02-09 16:27:30 -0600

    CARLI-289 Added schema and instance method (plus unit tests) for offering flagged state

--- CARLI/test/Offering.spec.js
+++ CARLI/test/Offering.spec.js
@@ -70,5 +70,76 @@ function runOfferingSpecificTests(testCycle) {
 
             it('should list offerings for a specific product');
         });
+
+        describe('Adding functions to Offerings instances', function () {
+            it('should add a getFlaggedState method to instances of Offerings', function() {
+                return OfferingRepository.create(validOfferingData(),testCycle)
+                    .then(function(offeringId){
+                        return OfferingRepository.load(offeringId,testCycle);
+                    })
+                    .then(function(offering){
+                        return expect(offering.getFlaggedState).to.be.a('function');
+                    });
+            });
+
+            it('should return true if the offering has property set to true', function() {
+                var testOffering = validOfferingData();
+                testOffering.flagged = true;
+
+                return OfferingRepository.create(testOffering,testCycle)
+                    .then(function(offeringId){
+                        return OfferingRepository.load(offeringId,testCycle);
+                    })
+                    .then(function(offering){
+                        return expect(offering.getFlaggedState()).to.equal(true);
+                    });
+            });
+
+            it('should return false if the offering has property set to false', function() {
+                var testOffering = validOfferingData();
+                testOffering.flagged = false;
+
+                return OfferingRepository.create(testOffering,testCycle)
+                    .then(function(offeringId){
+                        return OfferingRepository.load(offeringId,testCycle);
+                    })
+                    .then(function(offering){
+                        return expect(offering.getFlaggedState()).to.equal(false);
+                    });
+            });
+
+            it('should compute false if the offering has valid pricing data', function() {
+                var testOffering = validOfferingData();
+
+                return OfferingRepository.create(testOffering,testCycle)
+                    .then(function(offeringId){
+                        return OfferingRepository.load(offeringId,testCycle);
+                    })
+                    .then(function(offering){
+                        return expect(offering.getFlaggedState()).to.equal(false);
+                    });
+            });
+
+            it('should compute true if the offering has an su price great than the site license price', function() {
+                var testOffering = validOfferingData();
+                testOffering.pricing = {
+                    site: 1000,
+                    su: [{
+                        users: 2,
+                        price: 2000
+                    }]
+                };
+
+                return OfferingRepository.create(testOffering,testCycle)
+                    .then(function(offeringId){
+                        return OfferingRepository.load(offeringId,testCycle);
+                    })
+                    .then(function(offering){
+                        return expect(offering.getFlaggedState()).to.equal(true);
+                    });
+            });
+        });
+
+
     });
 }
