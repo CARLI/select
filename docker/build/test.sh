2327603e8cd8a8ba7e13e75134e57ecfe5f2e703
Bryan <bryan@pixotech.com>
2015-02-20 13:55:48 -0600

    Don't ignore docker image named 'build'

---
+++ docker/build/test.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "Configuring CARLI for test instance"
+
+cd /carli-select && grunt jsenv:node
+cd /carli-select/jenkins && grunt generate-config:test
+
+cd /carli-select &&
+    CARLI_TEST_ENABLE_CHROME="no" CARLI_TEST_ENABLE_FIREFOX="yes" CARLI_TEST_ENABLE_SHARDING="no" grunt test:jenkins
