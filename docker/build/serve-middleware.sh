ebbb8e70937dc67d14626f3708ba2e10adadf523
Bryan <bryan@pixotech.com>
2015-02-23 14:00:36 -0600

    No need to install dependencies twice.

--- docker/build/serve-middleware.sh
+++ docker/build/serve-middleware.sh
@@ -6,5 +6,4 @@ echo "Configuring CARLI for deployment"
 
 cd /carli-select && grunt jsenv:node
 cd /carli-select/jenkins && grunt generate-config:$instance
-
 cd /carli-select/middleware && node ./index.js
