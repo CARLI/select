5f74658fcf6582c7f264289ff5c53acabaa44e9a
Bryan <bryan@pixotech.com>
2015-02-23 14:01:51 -0600

    Pass instance into middleware server wrapper

--- jenkins/scripts/run-middleware-container.sh
+++ jenkins/scripts/run-middleware-container.sh
@@ -11,4 +11,4 @@ sudo docker run \
     --volumes-from "carli-build-$instance" \
     -p 3000 \
     --workdir=/carli-select/middleware \
-    carli-build:$tag /carli-select/docker/build/serve-middleware.sh
+    carli-build:$tag /carli-select/docker/build/serve-middleware.sh $instance
