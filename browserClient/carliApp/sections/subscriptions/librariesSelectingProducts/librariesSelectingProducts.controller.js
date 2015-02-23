c9e05657ad1cb73fd29eb5ae8724e6e1f1f034bc
Lonesome Marty <marty@pixotech.com>
2015-02-23 11:57:41 -0600

    CARLI-431 Close system modal / confirmation dialog.

--- browserClient/carliApp/sections/subscriptions/librariesSelectingProducts/librariesSelectingProducts.controller.js
+++ browserClient/carliApp/sections/subscriptions/librariesSelectingProducts/librariesSelectingProducts.controller.js
@@ -1,16 +1,32 @@
 angular.module('carli.sections.subscriptions.librariesSelectingProducts')
     .controller('librariesSelectingProductsController', librariesSelectingProductsController);
 
-function librariesSelectingProductsController( $q, notificationService ) {
+function librariesSelectingProductsController( $q, alertService ) {
     var vm = this;
     vm.undoOpenSystem = undoOpenSystem;
     vm.closeSystem = closeSystem;
-
+    vm.closeSystemDialogComplete = closeSystemDialogComplete;
+    
     function undoOpenSystem(){
         vm.cycleRouter.previous();
     }
 
     function closeSystem(){
-        vm.cycleRouter.next();
+        //modal submit calls openSystemDialogComplete
+        $('#close-system-modal').modal('show');
+    }
+
+    function closeSystemDialogComplete(){
+        closeModal().then(vm.cycleRouter.next);
+    }
+
+    function closeModal() {
+        var deferred = $q.defer();
+        $('#close-system-modal').modal('hide');
+        $('#close-system-modal').on('hidden.bs.modal', function (e) {
+            alertService.putAlert('System closed', {severity: 'success'});
+            deferred.resolve();
+        });
+        return deferred.promise;
     }
 }
