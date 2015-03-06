6d6bd07c68af4a59e643467f142fad7c0e109492
Bryan and Marty <bryan@pixotech.com>
2015-03-06 10:09:40 -0600

    CARLI-392 always specify a container tag (otherwise it tries to use "latest")

--- jenkins/scripts/run-content-container.sh
+++ jenkins/scripts/run-content-container.sh
@@ -13,23 +13,23 @@ echo "Installing build dependencies"
 sudo docker run --rm -t \
     --volumes-from=carli-build-$instance \
     --workdir=/carli-select \
-    carli-build ./install-dependencies.sh
+    carli-build:$tag ./install-dependencies.sh
 
 echo "Configuring Javascript environment for node"
 sudo docker run --rm -t \
     --volumes-from=carli-build-$instance \
     --workdir=/carli-select/config \
-    carli-build grunt jsenv:node
+    carli-build:$tag grunt jsenv:node
 
 echo "Generating config for $instance"
 sudo docker run --rm -t \
     --volumes-from=carli-build-$instance \
     -e "CARLI_CRM_MYSQL_PASSWORD=$CARLI_CRM_MYSQL_PASSWORD" \
     --workdir=/carli-select/jenkins \
-    carli-build grunt generate-config:$instance
+    carli-build:$tag grunt generate-config:$instance
 
 echo "Building browser clients"
 sudo docker run --rm -t \
     --volumes-from=carli-build-$instance \
     --workdir=/carli-select/browserClient \
-    carli-build grunt build
+    carli-build:$tag grunt build
