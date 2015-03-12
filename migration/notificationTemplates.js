1bb1ad4fe0fb83deebbe9f4c5aa786680f7f8d44
Bryan and Marty <bryan@pixotech.com>
2015-03-12 15:58:05 -0500

    CARLI-533 Add the canned notification templates as part of the migration

---
+++ migration/notificationTemplates.js
@@ -0,0 +1,69 @@
+var NotificationTemplateRepository = require('../CARLI').NotificationTemplate;
+var CouchDbStore = require('../CARLI').CouchDbStore;
+var carliConfig = require('../CARLI').config;
+var StoreOptions = carliConfig.storeOptions;
+var Store = require('../CARLI').Store;
+var Q = require('q');
+NotificationTemplateRepository.setStore(Store(CouchDbStore(StoreOptions)));
+
+var notificationTemplates = [
+    {
+        name: 'Library Invoices',
+        subject: 'CARLI Database Invoices',
+        emailBody: '',
+        pdfBody: '{ pdfContent }',
+        pdfContentIsEditable: true
+    },
+    {
+        name: 'Library Estimates - Open Cycle',
+        subject: 'Estimate: LIBRARY Selections',
+        emailBody: '',
+        pdfBody: '{ pdfContent }',
+        pdfContentIsEditable: true
+    },
+    {
+        name: 'Library Reminder',
+        subject: 'Reminder: CARLI Selection System Open',
+        emailBody: '',
+        pdfContentIsEditable: false
+    },
+    {
+        name: 'Library Estimates - Closed Cycle',
+        subject: 'Estimate: LIBRARY Selections',
+        emailBody: '',
+        pdfBody: '{ pdfContent }',
+        pdfContentIsEditable: true
+    },
+    {
+        name: 'Vendor Reports',
+        subject: 'Report: CARLI Library Selections',
+        emailBody: '',
+        pdfBody: '{ pdfContent }',
+        pdfContentIsEditable: false
+    },
+    {
+        name: 'Contact Non-Players',
+        subject: 'CARLI Selections',
+        emailBody: '',
+        pdfContentIsEditable: false
+    },
+    {
+        name: 'Annual Access Fee Invoices',
+        subject: 'CARLI Annual Access Fee Invoices',
+        emailBody: '',
+        pdfBody: '{ pdfContent }',
+        pdfContentIsEditable: false
+    }
+];
+
+function migrateNotificationTemplates(){
+    return Q.all( notificationTemplates.map(createNotificationTemplate));
+}
+
+function createNotificationTemplate( notificationTemplate ) {
+    return NotificationTemplateRepository.create(notificationTemplate);
+}
+
+module.exports = {
+    migrateNotificationTemplates: migrateNotificationTemplates
+};
