10cd9b1039e08ffd61def6048c25e607b219bbaa
Lonesome Marty <marty@pixotech.com>
2015-04-08 11:54:59 -0500

    CARLI-638 update the drafts count every 2s

--- browserClient/carliApp/components/userMenu/userMenu.controller.js
+++ browserClient/carliApp/components/userMenu/userMenu.controller.js
@@ -1,9 +1,23 @@
 angular.module('carli.userMenu')
 .controller('userMenuController', userMenuController);
 
-function userMenuController(){
+function userMenuController( $interval, notificationService ){
     var vm = this;
 
-    vm.notificationsCount = 2;
+    vm.notificationsCount = 0;
     vm.userName = 'Username!';
+
+    $interval(function() {
+        countDrafts().then(function(count) {
+            vm.notificationsCount = count;
+        });
+    }, 2000);
+
+
+    function countDrafts(){
+        return notificationService.listDrafts()
+            .then(function(draftsList){
+                return draftsList.length;
+            });
+    }
 }
