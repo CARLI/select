5d903e4a17bfca1f8774def603f1dd0882620d10
Bryan and Marty <bryan@pixotech.com>
2015-04-13 16:47:09 -0500

    CARLI-600 Populate the pricing grid with jQuery instead of a normal angular template.

--- browserClient/vendorApp/sections/siteLicensePrices/siteLicensePrices.controller.js
+++ browserClient/vendorApp/sections/siteLicensePrices/siteLicensePrices.controller.js
@@ -1,7 +1,7 @@
 angular.module('vendor.sections.siteLicensePrices')
     .controller('siteLicensePricesController', siteLicensePricesController);
 
-function siteLicensePricesController($q, libraryService, productService, currentUser){
+function siteLicensePricesController($q, $filter, libraryService, productService, currentUser){
     var vm = this;
     vm.loadingPromises = [];
     vm.viewOptions = {};
@@ -31,12 +31,62 @@ function siteLicensePricesController($q, libraryService, productService, current
     }
 
     function loadProducts() {
-        return productService.listProductsForVendorId( currentUser.vendor.id ).then(function (products) {
-            vm.products = products;
+        return productService.listProductsWithOfferingsForVendorId( currentUser.vendor.id ).then(function (products) {
+            vm.products = $filter('orderBy')(products, 'name');
+
+            buildPriceArray();
+            buildPricingGrid();
         });
     }
 
-    function getPrice(){
-        return '';
+    function buildPriceArray() {
+
+        vm.priceForLibraryByProduct = {};
+
+        vm.products.forEach(function (product) {
+            vm.priceForLibraryByProduct[product.id] = {};
+
+            product.offerings.forEach(function (offering) {
+                vm.priceForLibraryByProduct[product.id][offering.library.id] = offering.pricing.site;
+            });
+        });
+    }
+
+    function getPrice(product, library){
+        var offeringsForLibrary = product.offerings.filter(function (offering) {
+            return offering.library.id == library.id;
+        });
+        return (offeringsForLibrary.length > 0) ? offeringsForLibrary[0].pricing.site : 0;
+    }
+
+    function buildPricingGrid() {
+        /*
+         <div class="price-row" ng-repeat="library in ::vm.libraries">
+         <div class="column" ng-repeat="product in ::vm.products | orderBy:'name'">
+         <input type="text" value="{{ ::vm.priceForLibraryByProduct[product.id][library.id] }}">
+         </div>
+         </div>
+         */
+        var priceRows = $('<div>');
+
+
+        vm.libraries.forEach(function (library) {
+            var row = generateLibraryRow();
+            vm.products.forEach(function (product) {
+                row.append( generateProductCell(library, product));
+            });
+            priceRows.append(row);
+        });
+
+        $('#price-rows').replaceWith(priceRows);
+
+        function generateLibraryRow() {
+            return $('<div class="price-row">');
+        }
+        function generateProductCell(library, product) {
+            var price = vm.priceForLibraryByProduct[product.id][library.id] || 0;
+            return $('<div class="column">').append('<input type="text" value="' + price + '">');
+        }
+
     }
 }
