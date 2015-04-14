efed0535607441a6ad1803e9a04010c38c068b04
Bryan and Marty <marty@pixotech.com>
2015-04-14 11:21:27 -0500

    CARLI-600 read prices out of grid and save changed offerings and create new offerings.

--- browserClient/vendorApp/sections/siteLicensePrices/siteLicensePrices.controller.js
+++ browserClient/vendorApp/sections/siteLicensePrices/siteLicensePrices.controller.js
@@ -1,11 +1,13 @@
 angular.module('vendor.sections.siteLicensePrices')
     .controller('siteLicensePricesController', siteLicensePricesController);
 
-function siteLicensePricesController($q, $filter, libraryService, productService, currentUser){
+function siteLicensePricesController($q, $filter, cycleService, libraryService, offeringService, productService, currentUser){
     var vm = this;
-    vm.loadingPromises = [];
+    vm.loadingPromise = null;
     vm.viewOptions = {};
-    
+
+    vm.saveOfferings = saveOfferings;
+
     activate();
 
     function activate() {
@@ -36,13 +38,13 @@ function siteLicensePricesController($q, $filter, libraryService, productService
 
     function buildPriceArray() {
 
-        vm.priceForLibraryByProduct = {};
+        vm.offeringsForLibraryByProduct = {};
 
         vm.products.forEach(function (product) {
-            vm.priceForLibraryByProduct[product.id] = {};
+            vm.offeringsForLibraryByProduct[product.id] = {};
 
             product.offerings.forEach(function (offering) {
-                vm.priceForLibraryByProduct[product.id][offering.library.id] = offering.pricing.site;
+                vm.offeringsForLibraryByProduct[product.id][offering.library.id] = offering;
             });
         });
     }
@@ -53,7 +55,7 @@ function siteLicensePricesController($q, $filter, libraryService, productService
         vm.libraries.forEach(function (library) {
             var row = generateLibraryRow();
             vm.products.forEach(function (product) {
-                row.append( generateProductCell(library, product));
+                row.append( generateOfferingCell(library, product));
             });
             priceRows.append(row);
         });
@@ -63,9 +65,79 @@ function siteLicensePricesController($q, $filter, libraryService, productService
         function generateLibraryRow() {
             return $('<div class="price-row">');
         }
-        function generateProductCell(library, product) {
-            var price = vm.priceForLibraryByProduct[product.id][library.id] || 0;
-            return $('<div class="column">').append('<input type="text" value="' + price + '">');
+        function generateOfferingCell(library, product) {
+            var offering = vm.offeringsForLibraryByProduct[product.id][library.id] || { pricing: { site: 0 }};
+            var price = offering.pricing.site;
+            var offeringCell = $('<div class="column offering">').append('<input type="number" step=".01" min="0" value="' + price + '">');
+
+            offeringCell.data('libraryId', library.id);
+            offeringCell.data('productId', product.id);
+
+            return offeringCell;
         }
+    }
+
+    function saveOfferings(){
+        var cycle = cycleService.getCurrentCycle();
+        var changedOfferings = [];
+        var newOfferings = [];
+        var offeringCells = $('#price-rows .offering');
+
+        offeringCells.each(function(index, element){
+            var offeringCell = $(element);
+            var libraryId = offeringCell.data('libraryId');
+            var productId = offeringCell.data('productId');
+            var offering = vm.offeringsForLibraryByProduct[productId][libraryId];
+            var newPrice = parseFloat( offeringCell.find('input').val());
+
+            if ( !offering ){
+                if ( newPrice != 0 ){
+                    offering = generateNewOffering(libraryId, productId, cycle, newPrice);
+                    newOfferings.push(offering);
+                }
+            }
+            else if ( newPrice != offering.pricing.site ){
+                offering.pricing.site = newPrice;
+                changedOfferings.push(offering);
+            }
+        });
+
+        vm.loadingPromise = saveAllOfferings( newOfferings, changedOfferings );
+    }
+
+    function generateNewOffering(libraryId, productId, cycle, newPrice) {
+        return {
+            cycle: cycle,
+            library: libraryId.toString(),
+            product: productId,
+            pricing: {
+                site: newPrice
+            }
+        };
+    }
+
+    function saveAllNewOfferings( newOfferings ){
+        return $q.all(newOfferings.map(offeringService.create));
+    }
+
+    function saveAllChangedOfferings( changedOfferings ){
+        return $q.all(changedOfferings.map(offeringService.update));
+    }
+
+    function saveAllOfferings( newOfferings, changedOfferings ){
+        var deferred = $q.defer();
+
+        $q.all( [ saveAllNewOfferings(newOfferings), saveAllChangedOfferings(changedOfferings) ] )
+            .then(function(arrays){
+                var count = arrays[0].length + arrays[1].length;
+                console.log('saved '+count+' offerings');
+                deferred.resolve();
+            })
+            .catch(function(err){
+                console.log(err);
+                deferred.reject(err);
+            });
+
+        return deferred.promise;
     }
 }
