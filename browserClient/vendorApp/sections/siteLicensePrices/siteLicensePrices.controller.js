936fec6e8a0964a893b0fea7ddbb1d1519c2c505
Bryan <bryan@pixotech.com>
2015-04-20 14:47:38 -0500

    CARLI-601 Add product/library id as classes to grid elements, and use them to show/hide based on the quick pricing selections.  Do not save a price if it is hidden when the user clicks save.

--- browserClient/vendorApp/sections/siteLicensePrices/siteLicensePrices.controller.js
+++ browserClient/vendorApp/sections/siteLicensePrices/siteLicensePrices.controller.js
@@ -1,7 +1,7 @@
 angular.module('vendor.sections.siteLicensePrices')
     .controller('siteLicensePricesController', siteLicensePricesController);
 
-function siteLicensePricesController($q, $filter, cycleService, libraryService, offeringService, productService, currentUser){
+function siteLicensePricesController($scope, $q, $filter, cycleService, libraryService, offeringService, productService, currentUser){
     var vm = this;
     vm.loadingPromise = null;
     vm.viewOptions = {};
@@ -44,11 +44,23 @@ function siteLicensePricesController($q, $filter, cycleService, libraryService, 
         vm.libraries.forEach(function(library) {
             vm.selectedLibraryIds[library.id] = true;
         });
+        $scope.$watchCollection(getSelectedLibraryIds, updateVisibilityOfElementsWithEntityIdClasses);
+        function getSelectedLibraryIds() { return vm.selectedLibraryIds; }
     }
     function initializeSelectedProductIds() {
         vm.products.forEach(function(product) {
             vm.selectedProductIds[product.id] = true;
         });
+        $scope.$watchCollection(getSelectedProductIds, updateVisibilityOfElementsWithEntityIdClasses);
+        function getSelectedProductIds() { return vm.selectedProductIds; }
+    }
+    function updateVisibilityOfElementsWithEntityIdClasses(selectedEntities) {
+        if (selectedEntities) {
+            Object.keys(selectedEntities).forEach(function (entityId) {
+                var displayValue = selectedEntities[entityId] ? 'flex' : 'none';
+                $('.' + entityId).css('display', displayValue);
+            });
+        }
     }
 
 
@@ -69,7 +81,7 @@ function siteLicensePricesController($q, $filter, cycleService, libraryService, 
         var priceRows = $('<div>').attr('id','price-rows');
 
         vm.libraries.forEach(function (library) {
-            var row = generateLibraryRow();
+            var row = generateLibraryRow(library);
             vm.products.forEach(function (product) {
                 row.append( generateOfferingCell(library, product));
             });
@@ -78,16 +90,20 @@ function siteLicensePricesController($q, $filter, cycleService, libraryService, 
 
         $('#price-rows').replaceWith(priceRows);
 
-        function generateLibraryRow() {
-            return $('<div class="price-row">');
+        function generateLibraryRow(library) {
+            var row = $('<div class="price-row">');
+            row.addClass('' + library.id);
+            return row;
         }
         function generateOfferingCell(library, product) {
             var offering = vm.offeringsForLibraryByProduct[product.id][library.id] || { pricing: { site: 0 }};
             var price = offering.pricing.site;
-            var offeringCell = $('<div class="column offering input">').append('<input type="number" step=".01" min="0" value="' + price + '">');
+            var offeringCell = $('<div class="column offering input">')
+                .append('<input type="number" step=".01" min="0" value="' + price + '">');
 
             offeringCell.data('libraryId', library.id);
             offeringCell.data('productId', product.id);
+            offeringCell.addClass(product.id);
 
             return offeringCell;
         }
@@ -106,15 +122,17 @@ function siteLicensePricesController($q, $filter, cycleService, libraryService, 
             var offering = vm.offeringsForLibraryByProduct[productId][libraryId];
             var newPrice = parseFloat( offeringCell.find('input').val());
 
-            if ( !offering ){
-                if ( newPrice !== 0 ){
-                    offering = generateNewOffering(libraryId, productId, cycle, newPrice);
-                    newOfferings.push(offering);
+            if ($(element).is(":visible")) {
+                if ( !offering ){
+                    if ( newPrice !== 0 ){
+                        offering = generateNewOffering(libraryId, productId, cycle, newPrice);
+                        newOfferings.push(offering);
+                    }
                 }
-            }
-            else if ( newPrice != offering.pricing.site ){
-                offering.pricing.site = newPrice;
-                changedOfferings.push(offering);
+                else if ( newPrice != offering.pricing.site ){
+                    offering.pricing.site = newPrice;
+                    changedOfferings.push(offering);
+                }
             }
         });
 
