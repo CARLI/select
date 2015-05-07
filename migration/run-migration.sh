4e65e281149e29b57125b701bbca13c386913dbc
Bryan <bryan@pixotech.com>
2015-05-07 11:50:12 -0500

    Add comment explaining CARLI_MIGRATION_NODE_PATH

--- migration/run-migration.sh
+++ migration/run-migration.sh
@@ -1,5 +1,14 @@
 grunt deploy-app-db;
 
+# Because of a bug with keepalive sockets in node 0.12, the migration will get
+# dropped connections from Couch.  Version 0.10 does not have this problem.  If
+# your system has 0.12 installed, you can set the path to an alternate
+# installation of 0.10.x with the CARLI_MIGRATION_NODE_PATH environment
+# variable.
+
+# For example, on a Mac:
+# export CARLI_MIGRATION_NODE_PATH=/usr/local/Cellar/node/0.10.33/bin
+
 if [ -n "$CARLI_MIGRATION_NODE_PATH" ]; then
     PATH="$CARLI_MIGRATION_NODE_PATH:$PATH"
 fi
