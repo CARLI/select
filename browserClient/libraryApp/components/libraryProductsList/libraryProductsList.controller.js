12a6e33ac6f584bbbc9dafd68ded34143732e647
Lonesome Marty <marty@pixotech.com>
2015-05-28 15:54:20 -0500

    CARLI-860 Show selections on the dashboard for active cycles.

--- browserClient/libraryApp/components/libraryProductsList/libraryProductsList.controller.js
+++ browserClient/libraryApp/components/libraryProductsList/libraryProductsList.controller.js
@@ -1,12 +1,19 @@
 angular.module('library.libraryProductsList')
 .controller('libraryProductsListController', libraryProductsListController);
 
-function libraryProductsListController( cycleService ){
+function libraryProductsListController( controllerBaseService, cycleService ){
     var vm = this;
 
     vm.loadingPromise = null;
     vm.selectedOfferings = [];
+    vm.sortOptions = {
+        productName: 'product.name',
+        vendorName: ['product.vendor.name','product.name'],
+        funded: ['product.funded','product.name'],
+        pricing: ['pricing.site','product.name']
+    };
 
+    controllerBaseService.addSortable(vm, vm.sortOptions.productName);
     activate();
 
     function activate(){
