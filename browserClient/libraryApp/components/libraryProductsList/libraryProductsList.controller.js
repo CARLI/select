87cc269715a1d527cd96c965be5b689018ab6539
Lonesome Marty <marty@pixotech.com>
2015-05-28 16:16:27 -0500

    CARLI-860 more styles and tweaks for the library app dashboard.

--- browserClient/libraryApp/components/libraryProductsList/libraryProductsList.controller.js
+++ browserClient/libraryApp/components/libraryProductsList/libraryProductsList.controller.js
@@ -1,7 +1,7 @@
 angular.module('library.libraryProductsList')
 .controller('libraryProductsListController', libraryProductsListController);
 
-function libraryProductsListController( controllerBaseService, cycleService ){
+function libraryProductsListController( $q, controllerBaseService, cycleService, productService ){
     var vm = this;
 
     vm.loadingPromise = null;
@@ -9,8 +9,9 @@ function libraryProductsListController( controllerBaseService, cycleService ){
     vm.sortOptions = {
         productName: 'product.name',
         vendorName: ['product.vendor.name','product.name'],
-        funded: ['product.funded','product.name'],
-        pricing: ['pricing.site','product.name']
+        license: ['product.license.name','product.name'],
+        su: ['selection.users','product.name'],
+        cost: ['selection.price','product.name']
     };
 
     controllerBaseService.addSortable(vm, vm.sortOptions.productName);
@@ -18,9 +19,26 @@ function libraryProductsListController( controllerBaseService, cycleService ){
 
     function activate(){
         vm.loadingPromise = cycleService.listSelectionsForCycle( vm.cycle)
+            .then(populateProductsForOfferings)
             .then(function( offerings ){
                 vm.selectedOfferings = offerings;
             });
+
+        function populateProductsForOfferings( offeringsList ){
+            return $q.all(offeringsList.map(loadProduct));
+
+            function loadProduct(offering){
+                if (typeof offering.product.vendor == 'object' && offering.product.license == 'object') {
+                    return $q.when(offering);
+                } else {
+                    return productService.load(offering.product.id)
+                        .then(function(product){
+                            offering.product = product;
+                            return offering;
+                        });
+                }
+            }
+        }
     }
 
 }
