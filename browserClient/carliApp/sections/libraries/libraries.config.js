561b204c45967a652d089526d048714d9d99edbc
Lonesome Marty <marty@pixotech.com>
2015-06-01 16:22:10 -0500

    CARLI-950 Move modal dialog component to common.
    Add one to the SU screen to show progress of saving products

--- browserClient/carliApp/sections/libraries/libraries.config.js
+++ browserClient/carliApp/sections/libraries/libraries.config.js
@@ -2,7 +2,7 @@ angular.module('carli.sections.libraries',[
     'ngSanitize',
     'carli.sections.libraries.edit',
     'carli.entityForms.library',
-    'carli.modalDialog',
+    'common.modalDialog',
     'common.libraryService',
     'carli.entityList'
 ]);
--- browserClient/vendorApp/sections/simultaneousUserPrices/simultaneousUserPrices.config.js
+++ browserClient/vendorApp/sections/simultaneousUserPrices/simultaneousUserPrices.config.js
@@ -2,6 +2,7 @@ angular.module('vendor.sections.simultaneousUserPrices',[
     'cgBusy',
     'common.checkbox',
     'common.libraryService',
+    'common.modalDialog',
     'common.productService',
     'vendor.horizontalScrollSlider',
     'vendor.quickPricing'
