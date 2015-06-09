a4343264a62568895c878e89d247fcb3520f312f
Marty <marty@pixotech.com>
2015-06-09 10:57:02 -0500

    CARLI-801 checklist

--- browserClient/vendorApp/sections/dashboard/dashboard.controller.js
+++ browserClient/vendorApp/sections/dashboard/dashboard.controller.js
@@ -1,6 +1,48 @@
 angular.module('vendor.sections.dashboard')
 .controller('dashboardController', dashboardController);
 
-function dashboardController(){
+function dashboardController($scope, cycleService, userService, vendorStatusService){
     var vm = this;
+    var currentUser = userService.getUser();
+    var cycle = cycleService.getCurrentCycle();
+    var computeObjectEqualityDeeply = true;
+
+    vm.vendorStatus = null;
+
+    vm.updateVendorStatus = updateVendorStatus;
+
+    activate();
+
+
+    function activate(){
+        vm.vendor = currentUser.vendor;
+        loadVendorStatus()
+            .then(watchVendorStatusChecklist);
+    }
+
+    function loadVendorStatus(){
+        return vendorStatusService.getStatusForVendor(vm.vendor.id, cycle)
+            .then(function(vendorStatus){
+                vm.vendorStatus = vendorStatus;
+            });
+    }
+
+    function watchVendorStatusChecklist(){
+        return $scope.$watch('vm.vendorStatus.checklist', updateVendorStatus, computeObjectEqualityDeeply);
+    }
+
+    function updateVendorStatus(value){
+        if ( !value ){
+            return;
+        }
+
+        return vendorStatusService.update(vm.vendorStatus, cycle)
+            .then(function(){
+                console.log('vendor status updated');
+                return loadVendorStatus();
+            })
+            .catch(function(err){
+                console.log('error updating vendor status', err);
+            });
+    }
 }
