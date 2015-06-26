de200c7cede7105b389b14acaeb8cffb77eb42c4
Marty <marty@pixotech.com>
2015-06-26 14:55:50 -0500

    CARLI-368 Don't create vendor databases or vendor statuses for archived cycles. Skip the statuses for the One-Time Purchase cycle too.

--- migration/vendorDatabases.js
+++ migration/vendorDatabases.js
@@ -1,6 +1,6 @@
 
 var Q = require('q');
-
+var carliConfig = require('../config');
 var cycleRepository = require('../CARLI/Entity/CycleRepository');
 var cycleRepositoryForVendor = require('../CARLI/Entity/CycleRepositoryForVendor');
 var vendorRepository = require('../CARLI/Entity/VendorRepository');
@@ -8,19 +8,27 @@ var vendorStatusRepository = require('../CARLI/Entity/VendorStatusRepository');
 
 function createVendorDatabases(cycleId) {
 
-    return vendorRepository.list().then(function (vendors) {
-        console.log('Replicating ' + cycleId + ' for ' + vendors.length + ' vendors');
-        return Q.all( vendors.map(createDatabase) );
+    return cycleRepository.load(cycleId)
+        .then(function(cycle){
+            if ( cycle.isArchived ){
+                return Q();
+            }
+            else {
+                return vendorRepository.list().then(function (vendors) {
+                    console.log('Replicating ' + cycleId + ' for ' + vendors.length + ' vendors');
+                    return Q.all( vendors.map(createDatabase) );
 
-        function createDatabase(vendor) {
-            var repoForVendor = cycleRepositoryForVendor(vendor);
-            return repoForVendor.createDatabase(cycleId)
-                .then(repoForVendor.load)
-                .then(function (cycleForVendor) {
-                    return cycleForVendor.replicateFromSource();
+                    function createDatabase(vendor) {
+                        var repoForVendor = cycleRepositoryForVendor(vendor);
+                        return repoForVendor.createDatabase(cycleId)
+                            .then(repoForVendor.load)
+                            .then(function (cycleForVendor) {
+                                return cycleForVendor.replicateFromSource();
+                            });
+                    }
                 });
-        }
-    });
+            }
+        });
 }
 
 function createVendorStatusesForAllCycles(){
@@ -32,11 +40,16 @@ function createVendorStatusesForAllCycles(){
 
     function ensureAllVendorsHaveStatusForCycle( cycle ){
 
-        return vendorRepository.list()
-            .then(function(vendorList){
-                console.log('Ensure vendor status entries for '+cycle.name+' - '+vendorList.length+' vendors');
-                return Q.all( vendorList.map(makeVendorStatus) );
-            });
+        if ( cycle.isArchived || cycle.id === carliConfig.oneTimePurchaseProductsCycleDocId ){
+            return Q();
+        }
+        else {
+            return vendorRepository.list()
+                .then(function(vendorList){
+                    console.log('Ensure vendor status entries for '+cycle.name+' - '+vendorList.length+' vendors');
+                    return Q.all( vendorList.map(makeVendorStatus) );
+                });
+        }
 
         function makeVendorStatus(vendor){
             var newStatus = {
