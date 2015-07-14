b67416bed9bdae78cc4d2c3257534f58aba57a3e
Marty <marty@pixotech.com>
2015-07-14 11:14:58 -0500

    CARLI-1085 Hook up library data to the historical pricing table.

--- browserClient/common/components/subscriptionHistoryTable/subscriptionHistoryTable.controller.js
+++ browserClient/common/components/subscriptionHistoryTable/subscriptionHistoryTable.controller.js
@@ -1,7 +1,7 @@
 angular.module('common.subscriptionHistoryTable')
     .controller('subscriptionHistoryTableController', subscriptionHistoryTableController);
 
-function subscriptionHistoryTableController(historicalPricingService){
+function subscriptionHistoryTableController(cycleService, historicalPricingService){
     var vm = this;
 
     vm.loadingPromise = null;
@@ -17,9 +17,52 @@ function subscriptionHistoryTableController(historicalPricingService){
             vm.yearLabel = 'Your History';
         }
 
-        vm.loadingPromise = historicalPricingService.getHistoricalPricingDataForProduct(vm.product.id, vm.cycle)
-            .then(function(historicalPricingData){
-                vm.rows = historicalPricingData;
+        vm.loadingPromise = loadDataForTemplate();
+    }
+
+    function loadDataForTemplate(){
+        if ( vm.library ){
+            return getHistoricalPricingAndSelectionData();
+        }
+        else {
+            return getHistoricalPricingDataOnly();
+        }
+    }
+
+    function getHistoricalPricingAndSelectionData(){
+        return getHistoricalPricingData()
+            .then(addSelectionData)
+            .then(saveHistoricPricingDataToVm);
+    }
+
+    function getHistoricalPricingDataOnly(){
+        return getHistoricalPricingData();
+    }
+
+    function getHistoricalPricingData(){
+        return historicalPricingService.getHistoricalPricingDataForProduct(vm.product.id, vm.cycle)
+            .then(saveHistoricPricingDataToVm);
+    }
+
+    function addSelectionData(historicPricingData){
+        return getSelectionDataForLibrary()
+            .then(function(historicSelectionData){
+                return historicPricingData.map(addSelectionDataToPricingData);
+
+                function addSelectionDataToPricingData(historicPricingEntry){
+                    historicPricingEntry.selected = historicSelectionData[historicPricingEntry.year];
+                    return historicPricingEntry;
+                }
             });
     }
+
+    function getSelectionDataForLibrary(){
+        return cycleService.getHistoricalSelectionDataForProductForCycle(vm.product.id, vm.cycle);
+    }
+
+    function saveHistoricPricingDataToVm(historicPricingData){
+        vm.rows = historicPricingData;
+        return historicPricingData;
+    }
+
 }
