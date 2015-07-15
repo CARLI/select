28876bf19ae7056d951e209f30fdb3331bcea9ec
Bryan <bryan@pixotech.com>
2015-07-15 10:45:52 -0500

    CARLI-1074 wire up session handling, refactor a little, clean up message

--- browserClient/common/components/auth/authTimeout.directive.js
+++ browserClient/common/components/auth/authTimeout.directive.js
@@ -1,21 +1,24 @@
 angular.module('common.auth')
     .directive('authTimeoutAlert', authTimeoutAlert);
 
-function authTimeoutAlert($interval, authTimeoutService, config) {
+function authTimeoutAlert($interval, authService, authTimeoutService, config) {
     return {
         restrict: 'E',
         template: [
             '<div class="underlay" ng-show="shouldShowAlert"></div>',
             '<div class="alert alert-danger" ng-show="shouldShowAlert">',
             '  <div class="message">',
-            '    <h1>Hey there!</h1>',
-            '    <p ng-show="inWarningPeriod">Your session will expire in {{ countDownString }}.</p>',
-            '    <p ng-show="!inWarningPeriod">Your session has ended.</p>',
-            '    <p>Thought you\'d like to know.</p>',
+            '    <h1>Session Timeout</h1>',
+            '    <div>',
+            '    <p  ng-show="inWarningPeriod">Your session expires after 60 minutes of inactivity.  Any unsaved changes will be lost.</p>',
+            '    <p>If you\'re still working you may <a>continue your session</a>, or you may <a>log out</a> now.</p>',
+            '    </div>',
+            '    <p>Your session will expire in {{ countDownMessage }}.</p>',
+            '    <p ng-show="!inWarningPeriod">Your session has expired.</p>',
             '  </div>',
             '  <div class="actions pull-right">',
-            '    <button class="carli-button secondary-button" ng-click="">Sure, whatever.</button>',
-            '    <button class="carli-button primary-button" ng-click="">Wait! I\'m not done!</button>',
+            '    <button class="carli-button secondary-button" ng-click="deleteSession()">Log out now ({{ countDownTime }})</button>',
+            '    <button class="carli-button primary-button" ng-click="refreshSession()">Continue Session</button>',
             '  </div>',
             '</div>'
         ].join("\n"),
@@ -25,49 +28,116 @@ function authTimeoutAlert($interval, authTimeoutService, config) {
     function authTimeoutAlertLink(scope) {
         var timeoutPendingPromise = null;
 
-        scope.shouldShowAlert = false;
-        scope.inWarningPeriod = true;
-        scope.countDownString = '10 minutes';
+        scope.deleteSession = deleteSession;
+        scope.refreshSession = refreshSession;
 
-        scope.$watch( authTimeoutService.getTimeoutState, function(timeoutState) {
-            switch (timeoutState) {
+        link();
+
+        function link() {
+            enterState(authTimeoutService.getTimeoutState());
+
+            authService.requireSession().then(function() {
+                watchTimeoutState();
+            });
+        }
+
+        function initialState() {
+            scope.shouldShowAlert = false;
+            scope.inWarningPeriod = false;
+            scope.timedOut = false;
+            scope.countDownMessage = '';
+        }
+
+        function showWarning() {
+            scope.shouldShowAlert = true;
+            scope.inWarningPeriod = true;
+            timeoutPendingPromise = startWarningCounter();
+        }
+
+        function timeOut() {
+            scope.shouldShowAlert = true;
+            scope.inWarningPeriod = false;
+        }
+
+        function watchTimeoutState() {
+            return scope.$watch(authTimeoutService.getTimeoutState, function (timeoutState, previousState) {
+                console.log('watching', timeoutState, previousState);
+                if (timeoutState === previousState) {
+                    return;
+                }
+                enterState(timeoutState);
+            });
+        }
+
+        function enterState(state) {
+            switch (state) {
                 case authTimeoutService.timeoutStateWarningPeriod:
-                    scope.shouldShowAlert = true;
-                    scope.inWarningPeriod = true;
-                    timeoutPendingPromise = startWarningCounter();
+                    showWarning();
                     break;
                 case authTimeoutService.timeoutStateTimedOut:
-                    scope.shouldShowAlert = true;
-                    scope.inWarningPeriod = false;
+                    timeOut();
                     break;
                 default:
-                    scope.shouldShowAlert = false;
+                    initialState();
             }
-        });
+        }
 
         function startWarningCounter() {
             var expiresAtTime = new Date().getTime() + config.authMillisecondsUntilWarningAppears;
-            var twoMinutes = 1000 * 60 * 2;
 
             return $interval(updateWarningCounter, 500);
 
             function updateWarningCounter() {
-                var timeNow = new Date().getTime();
-                var secondsLeft = Math.floor((expiresAtTime - timeNow) / 1000);
+                var remaining = splitRemainingTime();
 
-                var minutesRemaining = Math.floor(secondsLeft / 60);
-                var secondsRemaining = secondsLeft - (60*minutesRemaining);
+                scope.countDownMessage = formatMessage();
+                scope.countDownTime = formatTime();
 
-                if (secondsRemaining < 10) {
-                    secondsRemaining = '0' + secondsRemaining;
+                function splitRemainingTime() {
+                    var totalSecondsRemaining = getTotalSecondsLeft();
+                    var minutesRemaining = Math.floor(totalSecondsRemaining / 60);
+                    var secondsRemaining = getSecondsLeft();
+
+                    return {
+                        minutes: minutesRemaining,
+                        seconds: secondsRemaining
+                    };
+
+                    function getSecondsLeft() {
+                        var seconds = totalSecondsRemaining - (60 * minutesRemaining);
+
+                        if (seconds < 10) {
+                            seconds = '0' + seconds;
+                        }
+                        return seconds;
+                    }
                 }
 
-                if (minutesRemaining > 2) {
-                    scope.countDownString = minutesRemaining + ' minutes';
-                } else {
-                    scope.countDownString = minutesRemaining + ':' + secondsRemaining + ' minutes';
+                function formatMessage() {
+                    if (remaining.minutes > 2) {
+                        return remaining.minutes + ' minutes';
+                    } else {
+                        return remaining.minutes + ':' + remaining.seconds + ' minutes';
+                    }
+                }
+
+                function formatTime() {
+                    return remaining.minutes + ':' + remaining.seconds;
                 }
             }
+
+            function getTotalSecondsLeft() {
+                var timeNow = new Date().getTime();
+                return Math.floor((expiresAtTime - timeNow) / 1000);
+            }
+        }
+
+        function deleteSession() {
+            return authService.deleteSession().then(initialState);
+        }
+
+        function refreshSession() {
+            return authService.refreshSession().then(initialState);
         }
     }
 }
