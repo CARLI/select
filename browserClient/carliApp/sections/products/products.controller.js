68ee85e9499ca47a0621e79538d3fe5c1a662573
Marty <marty@pixotech.com>
2015-07-15 15:19:45 -0500

    CARLI-225 re-work the products landing page. It needs a cycle in the route to make loading the page reliable.
    Added a route and default handling. The 'real' product listing page is at /product/cycleId. The default /product route will route to a default (either the current cycle if there is one, or the first cycle from the list of active ones).

--- browserClient/carliApp/sections/products/products.controller.js
+++ browserClient/carliApp/sections/products/products.controller.js
@@ -1,17 +1,38 @@
 angular.module('carli.sections.products')
 .controller('productsController', productController);
 
-function productController( $scope, $sce, cycleService, productService ){
+function productController($location, $routeParams, $scope, $sce, cycleService, productService ){
     var vm = this;
     vm.activeCycles = [];
-    vm.afterProductSubmit = populateProductList;
+    vm.afterProductSubmit = newProductSubmitted;
+    vm.cycleId = $routeParams.cycleId;
+    vm.newProduct = newProduct();
+
     activate();
 
     function activate() {
+        console.log('Products list page activate: cycleId = '+vm.cycleId);
+
         cycleService.listActiveCycles().then(function(activeCycles) {
             vm.activeCycles = activeCycles;
         });
+
         watchCurrentCycle();
+
+        if ( vm.cycleId ){
+            cycleService.load(vm.cycleId)
+                .then(function(cycle){
+                    cycleService.setCurrentCycle(cycle);
+                    completeActivation(cycle);
+                });
+        }
+        else {
+            routeToADefaultCycle();
+        }
+    }
+
+    function completeActivation(cycle){
+        vm.newProduct = newProduct(cycle);
     }
 
     function watchCurrentCycle() {
@@ -22,10 +43,42 @@ function productController( $scope, $sce, cycleService, productService ){
         });
     }
 
+    function routeToADefaultCycle(){
+        var currentCycle = cycleService.getCurrentCycle();
+
+        if ( currentCycle ){
+            console.log('  route to current cycle '+currentCycle.name);
+            $location.path('/product/'+currentCycle.id);
+        }
+        else {
+            return cycleService.listActiveCycles()
+                .then(function (activeCycles) {
+                    var cycleId = activeCycles[0].id;
+                    console.log('  route to default cycle ' + cycleId);
+                    $location.path('/product/' + cycleId);
+                });
+        }
+    }
+
+    function newProductSubmitted() {
+        vm.newProduct = newProduct(cycle);
+        populateProductList();
+    }
+
     function populateProductList() {
         vm.loadingPromise = productService.list().then( function(productList){
             vm.productList = productList;
         });
+    }
+
+    function newProduct(cycle){
+        return {
+            type: 'Product',
+            cycle: cycle || cycleService.getCurrentCycle(),
+            isActive: true,
+            contacts: [],
+            futurePriceCaps: {}
+        };
     }
 
     vm.productListColumns = [
@@ -33,7 +86,7 @@ function productController( $scope, $sce, cycleService, productService ){
             label: "Product Name",
             orderByProperty: 'name',
             contentFunction: function(product) {
-                return $sce.trustAsHtml('<a href="product/' + product.id + '">' + productService.getProductDisplayName(product) + '</a>');
+                return $sce.trustAsHtml('<a href="product/' + vm.cycleId + '/' + product.id + '">' + productService.getProductDisplayName(product) + '</a>');
             }
         },
         {
