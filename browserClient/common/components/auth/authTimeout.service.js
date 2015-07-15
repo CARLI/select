20abc3797db4aac7bc0ee65d0a9ad18f1ba32e13
Bryan <bryan@pixotech.com>
2015-07-15 09:14:55 -0500

    CARLI-1074 Initial authTimeout service and directive

---
+++ browserClient/common/components/auth/authTimeout.service.js
@@ -0,0 +1,54 @@
+angular.module('common.auth')
+    .service('authTimeoutService', authTimeoutService);
+
+function authTimeoutService($timeout, authService, config) {
+    var authTimeoutWarningPromise = null;
+    var authTimeoutPromise = null;
+
+    var timeoutStateDefault = 'default';
+    var timeoutStateWarningPeriod = 'warning';
+    var timeoutStateTimedOut = 'timeout';
+
+    var currentState = timeoutStateDefault;
+
+    activate();
+
+    return {
+        timeoutStateWarningPeriod: timeoutStateWarningPeriod,
+        timeoutStateTimedOut: timeoutStateTimedOut,
+
+        getTimeoutState: getTimeoutState
+    };
+
+    function activate() {
+        startAuthTimeoutWarningCountdown();
+    }
+
+    function getTimeoutState() {
+        return currentState;
+    }
+
+    function startAuthTimeoutWarningCountdown() {
+        stopAuthTimeoutWarningCountdown();
+        authTimeoutWarningPromise = $timeout(enterWarningState, config.authTimeoutWarning);
+    }
+
+    function stopAuthTimeoutWarningCountdown() {
+        currentState = timeoutStateDefault;
+
+        if (authTimeoutWarningPromise) {
+            $timeout.cancel(authTimeoutWarningPromise);
+            authTimeoutWarningPromise = null;
+        }
+    }
+
+    function enterWarningState() {
+        authTimeoutWarningPromise = null;
+        authTimeoutPromise = $timeout(enterTimeoutState, config.authTimeout);
+        currentState = timeoutStateWarningPeriod;
+    }
+
+    function enterTimeoutState() {
+        currentState = timeoutStateTimedOut;
+    }
+}
