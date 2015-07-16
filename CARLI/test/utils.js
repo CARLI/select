b98bd22cf724045ca3106ee520b8ad02a765d1f3
Bryan <bryan@pixotech.com>
2015-07-16 13:59:56 -0500

    CARLI-913 allow optional cycle to be passed into getFlaggedState, to be used for unexpanded offerings

--- CARLI/test/utils.js
+++ CARLI/test/utils.js
@@ -62,7 +62,6 @@ module.exports = {
     deleteTestDbs: function() {
         var deferred = Q.defer();
 
-        console.log(testStoreOptions.privilegedCouchDbUrl);
         request.get(testStoreOptions.privilegedCouchDbUrl + '/_all_dbs', function (error, response, body) {
             if (error) {
                 deferred.reject(error);
@@ -94,7 +93,6 @@ module.exports = {
     deleteTestReplicators: function() {
         var deferred = Q.defer();
 
-        console.log(testStoreOptions.privilegedCouchDbUrl);
         request.get(testStoreOptions.privilegedCouchDbUrl + '/_replicator/_all_docs?include_docs=true', function (error, response, body) {
             var parsedBody = JSON.parse(body);
             var err = error || parsedBody.error;
