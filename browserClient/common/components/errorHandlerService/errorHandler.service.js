8d1d740beaf2bca8e7df02b007cd2c2f150e89b4
Marty <marty@pixotech.com>
2015-07-21 11:40:33 -0500

    CARLI-882 Add some specific error messages as well as a global 'back-end down' message

--- browserClient/common/components/errorHandlerService/errorHandler.service.js
+++ browserClient/common/components/errorHandlerService/errorHandler.service.js
@@ -7,14 +7,26 @@ function returnErrorHandlerFunction($rootScope, $location, alertService, authSer
     function handleError( error ){
         if (error.statusCode === 401) {
             handleAuthFailure();
-        } else {
+        }
+        else if ( error.statusCode === 502 ){
+            handleBackendUnavailable();
+        }
+        else {
             console.log('handled error ', error);
-            alertService.putAlert( error, { severity: 'danger' } );
+            showErrorToUser(error);
         }
     }
 
     function handleAuthFailure() {
         $rootScope.returnTo = $location.url();
         authService.deleteSession();
+    }
+
+    function handleBackendUnavailable(error){
+        showErrorToUser('The database is currently not reachable. This is an error that makes the system inoperable. Please notify CARLI.');
+    }
+
+    function showErrorToUser(message){
+        alertService.putAlert(message, { severity: 'danger' });
     }
 }
