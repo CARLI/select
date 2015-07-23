0a092b8fcea1f9f85f23dca59ac3cdd6bd3f8e34
Marty <marty@pixotech.com>
2015-07-23 15:54:03 -0500

    CARLI-1094 MiddlewareRequest assumes JSON responses.

--- middleware/browserAdapters/invoiceNumberGeneration.js
+++ middleware/browserAdapters/invoiceNumberGeneration.js
@@ -1,17 +1,17 @@
 var middlewareRequest = require('./middlewareRequest');
 
 function generateNextBatchId(){
-    return middlewareRequest({
-        path: '/next-batch-id',
-        method: 'get'
-    });
+    return middlewareRequest( {path: '/next-batch-id', method: 'get'} )
+        .then(getResultFromJson);
 }
 
 function generateNextInvoiceNumber(){
-    return middlewareRequest({
-        path: '/next-invoice-number',
-        method: 'get'
-    });
+    return middlewareRequest( {path: '/next-invoice-number', method: 'get'} )
+        .then(getResultFromJson);
+}
+
+function getResultFromJson(jsonResult){
+    return jsonResult.result;
 }
 
 module.exports = {
