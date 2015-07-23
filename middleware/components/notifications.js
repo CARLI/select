54254116ba68cc365d0574509696ea8f5707c816
Bryan <bryan@pixotech.com>
2015-07-23 15:29:37 -0500

    CARLI-388 send password reset emails

--- middleware/components/notifications.js
+++ middleware/components/notifications.js
@@ -1,6 +1,10 @@
+var handlebars = require('handlebars');
 var mailer = require('nodemailer');
-var request = require('request');
 var mailTransport = mailer.createTransport(null);
+var Q = require('q');
+var request = require('request');
+
+var config = require('../../config');
 
 function tellPixobot(envelope) {
     if (typeof envelope === 'string') {
@@ -13,23 +17,45 @@ function tellPixobot(envelope) {
     });
 }
 
-function sendMail() {
-    mailTransport.sendMail({
-        from: 'yoda@pixotech.com',
-        to: 'carli@pixotech.com',
-        subject: 'May the force be with you',
-        text: 'Easy, sending email from node is.',
-        html: '<img src="cid:yoda.jpg">' +
-        '<p>Easy, sending email from node is.</p>',
-        attachments: [
-            {
-                cid: 'yoda.jpg',
-                path: './yoda.jpg'
-            }
-        ]
-    });
+function sendNotification(to, template, variables) {
+    var realTo = config.notifications.overrideTo ? config.notifications.overrideTo : to;
+    var emailBodyText = fillTemplate(template, variables);
+
+    var options = {
+        from: config.notifications.from,
+        to: realTo,
+        subject: 'Reset your CARLI Select password',
+        text: emailBodyText
+    };
+
+    return sendMail(options);
+}
+
+function fillTemplate(template, variables) {
+    var compiled = handlebars.compile(template);
+    return compiled(variables);
+}
+
+function sendMail(options) {
+    var deferred = Q.defer();
+    mailTransport.sendMail(options, sendMailCallback);
+    return deferred;
+
+    function sendMailCallback(err, info) {
+        if (err)
+            deferred.reject(err);
+        else
+            deferred.resolve(info);
+    }
+    //mailTransport.sendMail({
+    //    from: 'yoda@pixotech.com',
+    //    to: 'carli@pixotech.com',
+    //    subject: 'May the force be with you',
+    //    text: 'Easy, sending email from node is.',
+    //});
 }
 
 module.exports = {
-    tellPixobot: tellPixobot
+    tellPixobot: tellPixobot,
+    sendNotification: sendNotification
 };
