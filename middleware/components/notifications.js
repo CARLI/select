e261f400b9a4aaf6a41d92356a3f9d4b3323c5be
Bryan <bryan@pixotech.com>
2015-07-23 11:38:41 -0500

    CARLI-388 begin to move test into notification middleware

--- middleware/components/notifications.js
+++ middleware/components/notifications.js
@@ -1,6 +1,11 @@
+var mailer = require('nodemailer');
 var request = require('request');
+var mailTransport = mailer.createTransport(null);
 
 function tellPixobot(envelope) {
+    if (typeof envelope === 'string') {
+        envelope = { message: envelope };
+    }
     request({
         url: 'http://pixobot.herokuapp.com/hubot/message-room/37097_carli@conf.hipchat.com',
         method: 'post',
@@ -8,6 +13,23 @@ function tellPixobot(envelope) {
     });
 }
 
+function sendMail() {
+    mailTransport.sendMail({
+        from: 'yoda@pixotech.com',
+        to: 'carli@pixotech.com',
+        subject: 'May the force be with you',
+        text: 'Easy, sending email from node is.',
+        html: '<img src="cid:yoda.jpg">' +
+        '<p>Easy, sending email from node is.</p>',
+        attachments: [
+            {
+                cid: 'yoda.jpg',
+                path: './yoda.jpg'
+            }
+        ]
+    });
+}
+
 module.exports = {
     tellPixobot: tellPixobot
-}
+};
