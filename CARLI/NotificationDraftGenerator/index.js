6feea0da78ef1f9ccab76934c1e1fe06a6fc2a2d
Marty <marty@pixotech.com>
2015-08-13 10:34:57 -0500

    CARLI-1127 Only load libraries with selections when estimating all libraries

--- CARLI/NotificationDraftGenerator/index.js
+++ CARLI/NotificationDraftGenerator/index.js
@@ -48,13 +48,7 @@ function getAnnualAccessFeeDraftForOneLibrary(template, notificationData) {
 function getAnnualAccessFeeDraftForAllLibraries(template, notificationData) {
 
     function getEntitiesForAnnualAccessFeeDraftForAllLibraries() {
-        return cycleRepository.load(config.oneTimePurchaseProductsCycleDocId)
-            .then(function(cycle){
-                return libraryRepository.listLibrariesWithSelectionsInCycle(cycle);
-            })
-            .then(function(libraryIds){
-                return libraryRepository.getLibrariesById(libraryIds);
-            });
+        return loadLibrariesWithSelectionsInCycle(config.oneTimePurchaseProductsCycleDocId);
     }
     function getRecipientsForAnnualAccessFeeDraftForAllLibraries() {
         return annualAccessAllLibrariesDraft.getEntities()
@@ -285,13 +279,7 @@ function getVendorReportsForOne(template, notificationData) {
 
 function getLibraryInvoicesForAll(template, notificationData) {
     function getEntitiesForLibraryInvoicesForAll() {
-        return cycleRepository.load(notificationData.cycleId)
-            .then(function(cycle) {
-                return libraryRepository.listLibrariesWithSelectionsInCycle(cycle);
-            })
-            .then(function (libraryIds) {
-                return libraryRepository.getLibrariesById(libraryIds);
-            });
+        return loadLibrariesWithSelectionsInCycle(notificationData.cycleId);
     }
     function getRecipientsForLibraryInvoicesForAll() {
         return allLibrariesDraft.getEntities()
@@ -419,7 +407,7 @@ function getLibraryInvoicesForOne(template, notificationData) {
 
 function getLibraryEstimatesForAll(template, notificationData) {
     function getEntitiesForLibraryEstimatesForAll() {
-        return libraryRepository.listActiveLibraries();
+        return loadLibrariesWithSelectionsInCycle(notificationData.cycleId);
     }
     function getRecipientsForLibraryEstimatesForAll() {
         return allLibrariesDraft.getEntities()
@@ -679,6 +667,16 @@ function onlyPurchasedOfferings(offering) {
 }
 function onlyOfferingsWithFees(offering) {
     return offering.product.oneTimePurchaseAnnualAccessFee;
+}
+
+function loadLibrariesWithSelectionsInCycle( cycleId ){
+    return cycleRepository.load(cycleId)
+        .then(function(cycle) {
+            return libraryRepository.listLibrariesWithSelectionsInCycle(cycle);
+        })
+        .then(function (libraryIds) {
+            return libraryRepository.getLibrariesById(libraryIds);
+        });
 }
 
 module.exports = {
