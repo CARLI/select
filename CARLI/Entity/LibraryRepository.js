bdfaec6c5f050fe89043298f44540ead97818ae5
Marty <marty@pixotech.com>
2015-08-14 11:42:46 -0500

    Doing some work in these files - clean up the mess at the top first.

--- CARLI/Entity/LibraryRepository.js
+++ CARLI/Entity/LibraryRepository.js
@@ -1,15 +1,15 @@
-var CrmLibraryEntity = require('./CrmLibraryEntity')
-    , Entity = require('../Entity')
-    , EntityTransform = require( './EntityTransformationUtils')
-    , config = require( '../../config' )
-    , StoreOptions = config.storeOptions
-    , Store = require( '../Store' )
-    , StoreModule = require( '../Store/CouchDb/Store')
-    , CouchUtils = require( '../Store/CouchDb/Utils')()
-    , Q = require('q')
-    , Validator = require('../Validator')
-    , _ = require('lodash')
-    ;
+var CrmLibraryEntity = require('./CrmLibraryEntity');
+var Entity = require('../Entity');
+var EntityTransform = require('./EntityTransformationUtils');
+var config = require('../../config');
+var StoreOptions = config.storeOptions;
+var Store = require('../Store');
+var StoreModule = require('../Store/CouchDb/Store');
+var CouchUtils = require('../Store/CouchDb/Utils')();
+var Q = require('q');
+var Validator = require('../Validator');
+var _ = require('lodash');
+
 
 var CONTACT_CATEGORY_ESTIMATE = 'Subscription Contacts';
 var CONTACT_CATEGORY_INVOICE  = 'Invoice Contacts';
