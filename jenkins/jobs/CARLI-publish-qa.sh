915495ba3d5803858ca2fccfd402eb0d5f599580
Bryan <bryan@pixotech.com>
2015-08-17 09:41:41 -0500

    skeleton of undockerized qa

--- jenkins/jobs/CARLI-publish-qa.sh
+++ jenkins/jobs/CARLI-publish-qa.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
 
-# Change to root directory of the project
-cd `git rev-parse --show-toplevel`
+fail () {
+    echo Build Failed
+    echo $*
+    caller
+    exit 1
+}
 
-export CARLI_CRM_MYSQL_PASSWORD
-export CARLI_INSTANCE=qa
-export CARLI_DOCKER_TAG=last-good
-export CARLI_COUCHDB_HOST_PORT=9081
-export CARLI_NGINX_HOST_PORT=9080
+in-directory () {
+    directory=$1
+    command=$2
 
-. ./jenkins/scripts/functions.sh
+    cd $directory
+      $command
+      status=$?
+    cd - > /dev/null
 
-redeploy-carli
+    return $status
+}
+
+
+buildBrowserClients() {
+    in-directory browserClient "grunt build"
+}
+
+./install-dependencies.sh || fail "Failed to install dependencies"
+
+grunt jsenv:node || fail "Failed to set up javascript environment for node (1)"
+buildBrowserClient || fail "Failed to build browser client"
+
+grunt jsenv:node || fail "Failed to set up javascript environment for node"
+
