b59d713e56e51e4f6bf875373c03307adf942a21
Bryan <bryan@pixotech.com>
2015-08-20 11:45:11 -0500

    Run all the tests, not just browser ones

--- jenkins/jobs/CARLI-build-and-test.sh
+++ jenkins/jobs/CARLI-build-and-test.sh
@@ -10,5 +10,4 @@ cd `git rev-parse --show-toplevel`
 ./install-dependencies.sh || fail "Failed to install dependencies"
 grunt jsenv:node || fail "Failed to set up javascript environment for node (1)"
 
-buildBrowserClients || fail "Failed to build browser clients"
 runTests || fail "Tests Failed"
