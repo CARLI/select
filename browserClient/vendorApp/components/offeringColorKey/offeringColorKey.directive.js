894e4963891e72cdefffc318d253807fbeb65db9
Marty <marty@pixotech.com>
2015-08-21 15:19:23 -0500

    CARLI-1023 Add a color key to both pricing screens.

---
+++ browserClient/vendorApp/components/offeringColorKey/offeringColorKey.directive.js
@@ -0,0 +1,13 @@
+angular.module('vendor.offeringColorKey')
+    .directive('offeringColorKey', function(){
+        return {
+            restrict: 'E',
+            template: [
+                        '<div id="offering-key">',
+                        '    <div class="updated"><div class="box"></div><span>Update Price</span></div>',
+                        '    <div class="imported"><div class="box"></div><span>Last Year\'s Price</span></div>',
+                        '    <div class="flagged"><div class="box"></div><span>Flagged Price</span></div>',
+                        '</div>'
+            ].join('')
+        };
+    });
