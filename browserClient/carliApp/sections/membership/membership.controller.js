1416ccc09ff0cf500c4df84666cf141698a20f52
Marty <marty@pixotech.com>
2015-08-28 10:34:39 -0500

    CARLI-847 Route to current year, use route param for membership year information.

--- browserClient/carliApp/sections/membership/membership.controller.js
+++ browserClient/carliApp/sections/membership/membership.controller.js
@@ -1,8 +1,37 @@
 angular.module('carli.sections.membership')
 .controller('membershipController', membershipController);
 
-function membershipController(){
+function membershipController( $location, $routeParams, libraryService ){
     var vm = this;
 
-    vm.year = 2015;
+    vm.currentYear = new Date().getFullYear();
+    vm.libraries = [];
+    vm.loadingPromise = null;
+    vm.displayYear = null;
+
+    activate();
+
+    function activate(){
+        vm.displayYear = $routeParams.year;
+
+        if ( vm.displayYear ){
+            initializeMembershipData();
+        }
+        else {
+            routeToDefaultYear();
+        }
+    }
+
+    function routeToDefaultYear(){
+        $location.path('/membership/' + vm.currentYear);
+    }
+
+    function initializeMembershipData(){
+        vm.loadingPromise = libraryService.list()
+            .then(function(libraryList){
+                vm.libraries = libraryList;
+            });
+
+        return vm.loadingPromise;
+    }
 }
