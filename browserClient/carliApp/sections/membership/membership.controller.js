e8812e12826957a5cdb2009c4151549c66329bfd
Marty <marty@pixotech.com>
2015-08-28 10:19:41 -0500

    CARLI-847 Add the membership tab and section

---
+++ browserClient/carliApp/sections/membership/membership.controller.js
@@ -0,0 +1,8 @@
+angular.module('carli.sections.membership')
+.controller('membershipController', membershipController);
+
+function membershipController(){
+    var vm = this;
+
+    vm.year = 2015;
+}
