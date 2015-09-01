3e37b065b3639dee3195b2dfac24bfac637e1662
Marty <marty@pixotech.com>
2015-09-01 16:45:59 -0500

    CARLI-1261 Implement list products for vendor report

--- browserClient/carliApp/sections/reports/reports.controller.js
+++ browserClient/carliApp/sections/reports/reports.controller.js
@@ -56,6 +56,9 @@ function reportsController( csvExportService, cycleService, errorHandler, report
         },
         {
             name: 'List all Products for Vendor',
+            controls: {
+                cycle: 'all'
+            },
             columns: []
         },
         {
