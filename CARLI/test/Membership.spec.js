d93f2209dc167dac61bd4c50cd8e68275d89d474
Marty <marty@pixotech.com>
2015-09-04 15:17:48 -0500

    CARLI-1242 Add a method to fake up membership dues as offerings for notification generator and summary total

--- CARLI/test/Membership.spec.js
+++ CARLI/test/Membership.spec.js
@@ -1,5 +1,6 @@
 var chai = require('chai');
 var expect = chai.expect;
+var membershipRepository = require('../Entity/MembershipRepository');
 var test = require('./Entity/EntityInterface.spec');
 var testUtils = require('./utils');
 
@@ -8,7 +9,21 @@ testUtils.setupTestDb();
 function validMembershipData() {
     return {
         type: 'Membership',
-        year: 2000
+        year: 2000,
+        data: {
+            1: {
+                ishare: 100,
+                membership: 110
+            },
+            2: {
+                ishare: 200,
+                membership: 220
+            },
+            3: {
+                ishare: 300,
+                membership: 330
+            }
+        }
     };
 }
 
@@ -19,3 +34,19 @@ function invalidMembershipData() {
 }
 
 test.run('Membership', validMembershipData, invalidMembershipData);
+
+describe('the getMembershipDuesAsOfferings method', function(){
+    it('should be a function', function(){
+        expect(membershipRepository.getMembershipDuesAsOfferings).to.be.a('function');
+    });
+
+    it('should return an array of fake offerings', function(){
+        var testOfferings = membershipRepository.getMembershipDuesAsOfferings( validMembershipData() );
+        expect(testOfferings).to.be.an('array');
+
+        var offering0 = testOfferings[0];
+        expect(offering0.library).to.have.property('id', '1');
+        expect(offering0.cycle).to.have.property('year', 2000);
+        expect(offering0.pricing).to.have.property('ishare', 100);
+    });
+});
