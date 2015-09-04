d95145db77cc6feae203fea245efe51fdce58b0d
Marty <marty@pixotech.com>
2015-09-04 15:17:48 -0500

    CARLI-1242 Add a method to fake up membership dues as offerings for notification generator and summary total

--- CARLI/Entity/MembershipRepository.js
+++ CARLI/Entity/MembershipRepository.js
@@ -13,4 +13,25 @@ MembershipRepository.loadDataForYear = function (year) {
     return couchUtils.getCouchViewResultValues(StoreOptions.couchDbName, 'membershipByYear', year);
 };
 
+MembershipRepository.getMembershipDuesAsOfferings = function (membershipData) {
+    var libraryIds = Object.keys(membershipData.data);
+    var year = membershipData.year;
+
+    return libraryIds.map(function convertMembershipDuesToOffering(libraryId) {
+        var dues = membershipData.data[libraryId];
+        return {
+            cycle: {
+                year: year
+            },
+            library: {
+                id: libraryId
+            },
+            pricing: {
+                ishare: dues.ishare,
+                membership: dues.membership
+            }
+        }
+    });
+};
+
 module.exports = MembershipRepository;
