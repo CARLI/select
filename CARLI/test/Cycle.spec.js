564f64b2b57d3473b5a7410412052d1d8ceddc3c
Marty <marty@pixotech.com>
2015-09-04 11:13:20 -0500

    Clean up this mess

--- CARLI/test/Cycle.spec.js
+++ CARLI/test/Cycle.spec.js
@@ -1,18 +1,16 @@
-var test = require( './Entity/EntityInterface.spec' )
-    , chai = require( 'chai' )
-    , expect = chai.expect
-    , uuid = require( 'node-uuid' )
-    , chaiAsPromised = require( 'chai-as-promised' )
-    , CycleRepository = require( '../Entity/CycleRepository' )
-    , CycleMiddleware = require('../../middleware/components/cycleCreation')
-    , Entity = require('../Entity')
-    , ProductRepository = require( '../Entity/ProductRepository' )
-    , config = require('../../config')
-    , request = require('request')
-    , storeOptions = config.storeOptions
-    , testUtils = require('./utils')
-    , Q = require('q')
-;
+var chai = require( 'chai' );
+var config = require('../../config');
+var CycleRepository = require( '../Entity/CycleRepository' );
+var Entity = require('../Entity');
+var expect = chai.expect;
+var ProductRepository = require( '../Entity/ProductRepository' );
+var request = require('request');
+var storeOptions = config.storeOptions;
+var testUtils = require('./utils');
+var test = require( './Entity/EntityInterface.spec' );
+var uuid = require( 'node-uuid' );
+
+var Q = require('q');
 
 testUtils.setupTestDb();
 
