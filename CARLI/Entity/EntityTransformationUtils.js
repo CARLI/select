cb4d2bd49df7bb689a37fa44df4f6edf442c31c6
Marty <marty@pixotech.com>
2015-09-10 16:42:48 -0500

    CARLI-1246 Make saving library contacts work.

--- CARLI/Entity/EntityTransformationUtils.js
+++ CARLI/Entity/EntityTransformationUtils.js
@@ -32,31 +32,9 @@ function setEntityLookupStores( newStore ){
     repositories.vendor.setStore( newStore );
 }
 
-function removeEmptyContactsFromEntity(entity) {
-    if ( !entity.contacts ){
-        return;
-    }
-
-    var noEmptiesContactList = [];
-
-    for (var i = 0; i < entity.contacts.length; i++) {
-        contact = entity.contacts[i];
-
-        if ((contact.name && contact.name.length > 0) ||
-            (contact.email && contact.email > 0) ||
-            (contact.phoneNumber && contact.phoneNumber > 0)) {
-
-            noEmptiesContactList.push(contact);
-        }
-    }
-    entity.contacts = noEmptiesContactList;
-}
-
-
 function transformObjectForPersistence(entity, propertiesToTransform) {
     _replaceObjectsWithIds(entity, propertiesToTransform);
     _removeHelperFunctions(entity);
-    removeEmptyContactsFromEntity(entity);
     setDefaultValuesForEntity(entity);
 }
 
@@ -233,7 +211,6 @@ function convertDateObjectToString( dateObject ){
 setEntityLookupStores( Store( StoreModule(StoreOptions) ) );
 
 module.exports = {
-    removeEmptyContactsFromEntity: removeEmptyContactsFromEntity,
     transformObjectForPersistence: transformObjectForPersistence,
     expandObjectFromPersistence: expandObjectFromPersistence,
     expandListOfObjectsFromPersistence: expandListOfObjectsFromPersistence,
