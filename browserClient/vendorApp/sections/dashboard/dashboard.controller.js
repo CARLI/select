7e5aec27e5a8e50eed0e385861bb3ef1931d2eb7
Marty <marty@pixotech.com>
2015-09-11 11:13:07 -0500

    CARLI-1157 Add done button that marks vendor status 'Done Entering Prices'.

--- browserClient/vendorApp/sections/dashboard/dashboard.controller.js
+++ browserClient/vendorApp/sections/dashboard/dashboard.controller.js
@@ -1,14 +1,16 @@
 angular.module('vendor.sections.dashboard')
 .controller('dashboardController', dashboardController);
 
-function dashboardController($scope, authService, cycleService, vendorStatusService){
+function dashboardController($scope, authService, cycleService, userService, vendorStatusService){
     var vm = this;
     var currentUser = null;
     var cycle = cycleService.getCurrentCycle();
     var computeObjectEqualityDeeply = true;
 
     vm.vendorStatus = null;
+    vm.vendorDoneEnteringPrices = false;
 
+    vm.doneEnteringPrices = doneEnteringPrices;
     vm.updateVendorStatus = updateVendorStatus;
 
     activate();
@@ -27,6 +29,7 @@ function dashboardController($scope, authService, cycleService, vendorStatusServ
         return vendorStatusService.getStatusForVendor(vm.vendor.id, cycle)
             .then(function(vendorStatus){
                 vm.vendorStatus = vendorStatus;
+                checkIfVendorIsDoneEnteringPrices();
             });
     }
 
@@ -44,19 +47,37 @@ function dashboardController($scope, authService, cycleService, vendorStatusServ
         vm.statusUpdating = true;
 
         return vendorStatusService.update(vm.vendorStatus, cycle)
-            .then(function(){
-                return syncData()
-                    .then(loadVendorStatus);
-            })
+            .then(syncData)
             .catch(function(err){
                 console.log('error updating vendor status', err);
             })
             .finally(function() {
                 vm.statusUpdating = false;
+                return loadVendorStatus();
             });
     }
 
     function syncData(){
         return cycleService.syncDataBackToCarli();
+    }
+
+    function doneEnteringPrices(){
+        var vendorId = userService.getUser().vendor.id;
+        vm.statusUpdating = true;
+
+        return vendorStatusService.updateVendorStatusActivity('Done Entering Prices', vendorId, cycle)
+            .then(syncData)
+            .then(checkIfVendorIsDoneEnteringPrices)
+            .catch(function(err){
+                console.log('error updating vendor status', err);
+            })
+            .finally(function() {
+                vm.statusUpdating = false;
+                return loadVendorStatus();
+            });
+    }
+
+    function checkIfVendorIsDoneEnteringPrices() {
+        vm.vendorDoneEnteringPrices = (vm.vendorStatus.description === 'Done Entering Prices');
     }
 }
