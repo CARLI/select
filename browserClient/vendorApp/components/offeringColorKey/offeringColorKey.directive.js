cd2283dd16f0aa445ca96620887cce49c2744fdc
Marty <marty@pixotech.com>
2016-02-03 15:12:47 -0600

    CARLI-1472 I stupidly made offering key use an id even though I made it a re-usable component

--- browserClient/vendorApp/components/offeringColorKey/offeringColorKey.directive.js
+++ browserClient/vendorApp/components/offeringColorKey/offeringColorKey.directive.js
@@ -3,7 +3,7 @@ angular.module('vendor.offeringColorKey')
         return {
             restrict: 'E',
             template: [
-                        '<div id="offering-key">',
+                        '<div class="offering-key">',
                         '    <div class="updated"><div class="box"></div><span>Updated Price</span></div>',
                         '    <div class="imported"><div class="box"></div><span>Last Year\'s Price</span></div>',
                         '    <div class="flagged"><div class="box"></div><span>Flagged Price</span></div>',
