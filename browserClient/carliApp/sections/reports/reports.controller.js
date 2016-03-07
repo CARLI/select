a3c93999a6cfb27d3472476e58565339dd7160ce
Marty <marty@pixotech.com>
2016-03-07 16:59:26 -0600

    CARLI-1519 Add UI for selecting vendors, products, and libraries for the all-pricing report

--- browserClient/carliApp/sections/reports/reports.controller.js
+++ browserClient/carliApp/sections/reports/reports.controller.js
@@ -1,13 +1,21 @@
 angular.module('carli.sections.reports')
 .controller('reportsController', reportsController);
 
-function reportsController( csvExportService, cycleService, errorHandler, reportDataService ){
+function reportsController( $q, csvExportService, cycleService, libraryService, productService, reportDataService, vendorService ){
     var vm = this;
 
     vm.reportOptions = {};
     vm.reportRunningPromise = null;
     vm.cycleControlIsMissingValue = cycleControlIsMissingValue;
     vm.selectedReport = null;
+    vm.selectedVendors = [];
+
+    vm.loadVendorsPromise = loadVendorsPromise;
+    vm.loadLibrariesPromise = loadLibrariesPromise;
+    vm.loadProductsForVendors = loadProductsForVendors;
+    vm.reportWantsProductsAndCyclesAreSelected = reportWantsProductsAndCyclesAreSelected;
+    vm.reportWantsProductsAndVendorsAreSelected = reportWantsProductsAndVendorsAreSelected;
+    vm.reportWantsLibrariesAndCyclesAreSelected = reportWantsLibrariesAndCyclesAreSelected;
 
     /**
      * Reports
@@ -148,6 +156,7 @@ function reportsController( csvExportService, cycleService, errorHandler, report
                 csvExportService.browserDownloadCsv(csvContent, makeFilename());
             })
             .catch(function (err) {
+                vm.reportRunningPromise = null;
                 Logger.log('CSV generation failed', err);
             });
 
@@ -156,5 +165,110 @@ function reportsController( csvExportService, cycleService, errorHandler, report
         function makeFilename(){
             return 'CARLI-'+ reportName +'-report-' + new Date().toISOString().substr(0,16).replace('T','-');
         }
+    }
+
+    function reportWantsProductsAndCyclesAreSelected() {
+        return vm.selectedReport.controls.product && cyclesAreSelected();
+    }
+
+    function reportWantsProductsAndVendorsAreSelected() {
+        return vm.selectedReport.controls.product && vendorsAreSelected();
+    }
+
+    function reportWantsLibrariesAndCyclesAreSelected() {
+        return vm.selectedReport.controls.library && cyclesAreSelected();
+    }
+
+    function cyclesAreSelected() {
+        return vm.reportOptions.parameters.cycle && vm.reportOptions.parameters.cycle.length;
+    }
+
+    function vendorsAreSelected() {
+        return vm.selectedVendors.length;
+    }
+
+    function loadVendorsPromise() {
+        if ( !vm.loadingVendorPromise ) {
+            vm.loadingVendorPromise = vendorService.list()
+                .then(function(allVendors){
+                    vm.vendors = allVendors;
+                });
+        }
+        return vm.loadingVendorPromise;
+    }
+
+    function loadProductsForVendors() {
+        vm.products = [];
+
+        if ( !reportWantsProductsAndCyclesAreSelected() || !vendorsAreSelected() ){
+            return;
+        }
+
+        vm.loadingProductsPromise = loadProductsForSelectedVendorsForSelectedCycles()
+            .then(reduceToListOfUniqueProducts)
+            .then(function(products) {
+                vm.products = products;
+            });
+
+
+        function loadProductsForSelectedVendorsForSelectedCycles() {
+            var selectedCycles = getSelectedCycles();
+            var selectedVendors = vm.selectedVendors;
+            var promisesForProductsByVendor = selectedVendors.map(loadProductsForVendorForSelectedCycles);
+
+            var allProducts = flattenArraysOfArraysOfPromises(promisesForProductsByVendor);
+            return $q.all( allProducts );
+
+
+            function getSelectedCycles() {
+                return vm.cycles.filter(cycleIsSelected);
+
+                function cycleIsSelected(cycle) {
+                    return vm.reportOptions.parameters.cycle.indexOf(cycle.id) >= 0;
+                }
+            }
+
+            function loadProductsForVendorForSelectedCycles( vendorId ) {
+                return selectedCycles.map(function(cycle) {
+                    return productService.listProductsForVendorId(vendorId, cycle);
+                });
+            }
+
+            function flattenArraysOfArraysOfPromises(arrayOfArrays){
+                var flattenedArray = [];
+                arrayOfArrays.forEach(function(subArray){
+                    subArray.forEach(function(item) {
+                        flattenedArray.push(item);
+                    });
+                });
+                return flattenedArray;
+            }
+        }
+
+        function reduceToListOfUniqueProducts(arrayOfProductsPerCycle){
+            console.log('loaded ' + arrayOfProductsPerCycle.length + ' cycles of products products', arrayOfProductsPerCycle);
+            var uniqueProductsById = {};
+            arrayOfProductsPerCycle.forEach(function(arrayOfProducts) {
+                arrayOfProducts.forEach(function(product) {
+                    uniqueProductsById[product.id] = product;
+                });
+            });
+
+            var uniqueProducts = Object.keys(uniqueProductsById).map(function(productId){
+                return uniqueProductsById[productId];
+            });
+
+            return uniqueProducts;
+        }
+    }
+
+    function loadLibrariesPromise() {
+        if ( !vm.loadingLibrariesPromise ) {
+            vm.loadingLibrariesPromise = libraryService.list()
+                .then(function(allLibraries){
+                    vm.libraries = allLibraries;
+                });
+        }
+        return vm.loadingLibrariesPromise;
     }
 }
