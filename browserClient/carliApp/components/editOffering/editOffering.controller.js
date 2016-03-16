a1d459d5c7ac0ff9de8b5a5a24c8a49e16d991cd
Marty <marty@pixotech.com>
2016-03-16 11:03:59 -0500

    CARLI-1536 move editOfferingController to separate file to match standards

---
+++ browserClient/carliApp/components/editOffering/editOffering.controller.js
@@ -0,0 +1,90 @@
+angular.module('carli.editOffering')
+    .controller('editOfferingController', editOfferingController);
+
+function editOfferingController(activityLogService, alertService, cycleService, errorHandler, offeringService, productService) {
+    var vm = this;
+    vm.saveOffering = saveOffering;
+    vm.shouldShowColumn = shouldShowColumn;
+    vm.offeringDisplayOptions = offeringService.getOfferingDisplayOptions();
+    vm.userClickedFlag = userClickedFlag;
+    vm.getProductDisplayName = productService.getProductDisplayName;
+
+    var userTouchedFlag = false;
+
+    activate();
+
+
+    function activate() {
+        offeringService.load(vm.offering.id)
+            .then(workaroundCouchStoreRevisionSmell);
+    }
+
+    function saveOffering() {
+        var tempLibraryComments = "an arbitrary string that is very unlikely to be an actual product comment";
+        if (vm.offering.libraryComments === vm.offering.product.comments) {
+            tempLibraryComments = vm.offering.product.comments;
+            delete vm.offering.libraryComments;
+        }
+        if (!userTouchedFlag) {
+            delete vm.offering.flagged;
+        }
+        userTouchedFlag = false;
+
+        if ('selection' in vm.offering && typeof vm.offering.selection === 'undefined') {
+            delete vm.offering.selection;
+        }
+
+        keepValidSuPricingRows();
+
+        return offeringService.update(vm.offering)
+            .then(offeringService.load)
+            .then(function (updatedOffering) {
+                workaroundCouchStoreRevisionSmell(updatedOffering);
+                alertService.putAlert('Offering updated', {severity: 'success'});
+                vm.notifyParentOfSave(vm.offering);
+                return logUpdateActivity();
+            })
+            .then(syncData)
+            .then(replaceLibraryCommentsIfRemoved)
+            .catch(errorHandler);
+
+        function replaceLibraryCommentsIfRemoved() {
+            if (tempLibraryComments === vm.offering.product.comments) {
+                vm.offering.libraryComments = tempLibraryComments;
+            }
+        }
+
+        function syncData() {
+            return cycleService.syncDataToVendorDatabase(vm.offering.vendorId);
+        }
+
+        function keepValidSuPricingRows() {
+            var newSuPricing = [];
+
+            for (var i in vm.offering.pricing.su) {
+                var pricingRow = vm.offering.pricing.su[i];
+                if (pricingRow.price && pricingRow.users) {
+                    newSuPricing.push(vm.offering.pricing.su[i]);
+                }
+            }
+
+            vm.offering.pricing.su = newSuPricing;
+        }
+    }
+
+    function workaroundCouchStoreRevisionSmell(offering) {
+        vm.offering._rev = offering._rev;
+    }
+
+    function shouldShowColumn(columnName) {
+        return vm.columns.indexOf(columnName) !== -1;
+    }
+
+    function userClickedFlag() {
+        userTouchedFlag = true;
+    }
+
+    function logUpdateActivity() {
+        return activityLogService.logOfferingModified(vm.offering, vm.cycle);
+    }
+}
