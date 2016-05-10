9fd56e17a669f2a672d05a0489155766350e558e
Marty <marty@pixotech.com>
2016-05-10 09:55:44 -0500

    CARLI-1581 move some things from the top-level controller to the library controller

--- browserClient/carliApp/sections/oneTimePurchases/oneTimePurchases.controller.js
+++ browserClient/carliApp/sections/oneTimePurchases/oneTimePurchases.controller.js
@@ -6,55 +6,10 @@ function oneTimePurchasesController( $sce, notificationModalService, libraryServ
 
     vm.groupBy = 'library';
 
-    vm.invoiceAnnualAccessFees = invoiceAnnualAccessFees;
-
-    vm.libraryListColumns = [
-        {
-            label: "Institution Name",
-            orderByProperty: 'name',
-            contentFunction: function(library) {
-                return $sce.trustAsHtml('<a href="oneTimePurchases/' + library.id + '">' + library.name + '</a>');
-            }
-        },
-        {
-            label: "FTE",
-            orderByProperty: 'fte',
-            contentFunction: function(library) { return getFte(library); }
-        },
-        {
-            label: "Type",
-            orderByProperty: 'institutionType',
-            contentFunction: function(library) { return library.institutionType; }
-        },
-        {
-            label: "",
-            orderByProperty: false,
-            contentFunction: function(library) {
-                return $sce.trustAsHtml('<a class="carli-button list-button" href="oneTimePurchases/' + library.id + '">View <i class="fa fa-chevron-circle-right"></i></a>');
-            }
-        }
-    ];
-
     activate();
 
+
     function activate() {
-        vm.libraryLoadingPromise = libraryService.listActiveLibraries().then( function(libraryList){
-            vm.libraryList = libraryList;
-        });
-    }
-
-    function invoiceAnnualAccessFees() {
-        notificationModalService.sendStartDraftMessage({
-            templateId: 'notification-template-annual-access-fee-invoices'
-        });
-    }
-
-    function getFte( library ){
-        var result = library.fte;
-
-        if ( library.fteInfo ){
-            result += ' (' + library.fteInfo + ')';
-        }
-        return result;
+        console.log('oneTimePurchasesController activate');
     }
 }
