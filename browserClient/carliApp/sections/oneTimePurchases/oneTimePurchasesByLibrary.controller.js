22aed26d7e947db24c9609e4c1ef1f9e06747641
Marty <marty@pixotech.com>
2016-05-10 09:55:44 -0500

    CARLI-1581 move some things from the top-level controller to the library controller

--- browserClient/carliApp/sections/oneTimePurchases/oneTimePurchasesByLibrary.controller.js
+++ browserClient/carliApp/sections/oneTimePurchases/oneTimePurchasesByLibrary.controller.js
@@ -4,9 +4,18 @@ angular.module('carli.sections.oneTimePurchases')
 function oneTimePurchasesByLibraryController() {
     var vm = this;
 
+    vm.invoiceAnnualAccessFees = invoiceAnnualAccessFees;
+
     activate();
 
     function activate() {
         console.log('oneTimePurchasesByLibraryController activate');
     }
+
+    function invoiceAnnualAccessFees() {
+        notificationModalService.sendStartDraftMessage({
+            templateId: 'notification-template-annual-access-fee-invoices'
+        });
+    }
+
 }
