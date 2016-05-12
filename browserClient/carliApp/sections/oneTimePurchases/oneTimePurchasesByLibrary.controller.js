0758bde549cd75825f06e285ad84ab176bea7caa
Marty <marty@pixotech.com>
2016-05-12 11:36:14 -0500

    CARLI-1581 Add a bunch more functionality to the screen

--- browserClient/carliApp/sections/oneTimePurchases/oneTimePurchasesByLibrary.controller.js
+++ browserClient/carliApp/sections/oneTimePurchases/oneTimePurchasesByLibrary.controller.js
@@ -1,16 +1,24 @@
 angular.module('carli.sections.oneTimePurchases')
     .controller('oneTimePurchasesByLibraryController', oneTimePurchasesByLibraryController);
 
-function oneTimePurchasesByLibraryController(config, controllerBaseService, cycleService, libraryService, notificationModalService, vendorService) {
+function oneTimePurchasesByLibraryController($scope, accordionControllerMixin, config, controllerBaseService, cycleService, editOfferingService, libraryService, notificationModalService, offeringService, productService, vendorService) {
     var vm = this;
 
+    accordionControllerMixin(vm, loadOfferingsForLibrary);
+    controllerBaseService.addSortable(vm, 'product.name');
+
     vm.cycle = null;
-    vm.filterOfferingBySelection = filterOfferingBySelection;
-    vm.invoiceAnnualAccessFees = invoiceAnnualAccessFees;
+    vm.isEditing = {};
     vm.libraryList = [];
     vm.libraryListLoadingPromise = null;
+    vm.loadingPromise = {};
+    vm.offerings = {};
     vm.offeringFilter = {};
     vm.vendorMap = {};
+
+    vm.filterOfferingBySelection = filterOfferingBySelection;
+    vm.invoiceAnnualAccessFees = invoiceAnnualAccessFees;
+    vm.stopEditing = stopEditing;
 
     vm.offeringColumns = [
         'product',
@@ -19,14 +27,31 @@ function oneTimePurchasesByLibraryController(config, controllerBaseService, cycl
         'site_license_price_only_as_price',
         'selection'
     ];
+    vm.sortOptions = {
+        product: 'product.name',
+        vendor: ['product.vendor.name', 'product.name'],
+        pricing: orderBySitePricing
+    };
+
     activate();
 
     function activate() {
-
+        connectEditButtons();
 
         vm.libraryListLoadingPromise = initControllerData();
 
         return vm.libraryListLoadingPromise;
+    }
+
+    function connectEditButtons() {
+        $scope.$watch(editOfferingService.receiveOfferingEditableMessage, receiveOfferingEditableMessage);
+
+        function receiveOfferingEditableMessage(newOffering, oldOffering) {
+            if (newOffering) {
+                setOfferingEditable(newOffering);
+                editOfferingService.acknowledgeOfferingMadeEditable();
+            }
+        }
     }
 
     function filterOfferingBySelection(offering) {
@@ -82,11 +107,55 @@ function oneTimePurchasesByLibraryController(config, controllerBaseService, cycl
         }
     }
 
-
     function invoiceAnnualAccessFees() {
         notificationModalService.sendStartDraftMessage({
             templateId: 'notification-template-annual-access-fee-invoices'
         });
     }
 
+    function loadOfferingsForLibrary(library) {
+        if (vm.offerings[library.id]) {
+            return $q.when();
+        }
+
+        vm.loadingPromise[library.id] = offeringService.listOfferingsForLibraryId(library.id)
+            .then(attachVendorsToOfferingProducts)
+            .then(filterActiveProducts)
+            .then(attachOfferingsToLibrary);
+
+        return vm.loadingPromise[library.id];
+
+
+        function attachVendorsToOfferingProducts(offerings) {
+            return offerings.map(function (offering) {
+                var vendorId = offering.product.vendor;
+                offering.product.vendor = vm.vendorMap[vendorId];
+                return offering;
+            });
+        }
+
+        function filterActiveProducts(offerings) {
+            return offerings.filter(function (offering) {
+                return productService.isProductActive(offering.product);
+            });
+        }
+
+        function attachOfferingsToLibrary(offerings) {
+            vm.offerings[library.id] = offerings;
+            return offerings;
+        }
+    }
+
+    function orderBySitePricing(offering){
+        return offering.pricing.site || 0;
+    }
+    
+    function setOfferingEditable( offering ){
+        vm.isEditing[offering.id] = true;
+    }
+
+    function stopEditing(offering) {
+        vm.isEditing[offering.id] = false;
+        vm.notifyParentOfSave();
+    }
 }
