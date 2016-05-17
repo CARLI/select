91f51ed70d1ef34c21c51b72ffb1181f8224b2f4
Marty <marty@pixotech.com>
2016-05-17 10:09:10 -0500

    CARLI-1569 Bug fix: use the right repository method for library product exports

--- browserClient/common/components/libraryProductsList/libraryProductsList.controller.js
+++ browserClient/common/components/libraryProductsList/libraryProductsList.controller.js
@@ -66,7 +66,7 @@ function libraryProductsListController( $q, controllerBaseService, csvExportServ
                 offering.product.license.name,
                 offering.libraryComments,
                 offering.selection.users,
-                offeringService.getFundedSelectionPendingPrice(offering)
+                offeringService.getFundedSelectionPrice(offering)
             ];
         }
     }
