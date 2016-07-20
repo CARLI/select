2e574bc359e30055e018195709566610547de164
Marty <marty@pixotech.com>
2016-07-20 16:46:32 -0500

    CARLI-851 dashboard section 1/4: cycle list + totals

--- browserClient/carliApp/sections/dashboard/dashboard.controller.js
+++ browserClient/carliApp/sections/dashboard/dashboard.controller.js
@@ -1,6 +1,31 @@
 angular.module('carli.sections.dashboard')
-.controller('dashboardController', dashboardController);
+    .controller('dashboardController', dashboardController);
 
-function dashboardController(){
+function dashboardController($q, cycleService) {
     var vm = this;
+
+    vm.cycles = [];
+    vm.cyclesLoading = null;
+
+    vm.totalsByCycle = {};
+
+    activate();
+
+    function activate() {
+        vm.cyclesLoading = cycleService.listActiveSubscriptionCycles()
+            .then(function (activeCycles) {
+                vm.cycles = activeCycles;
+                return activeCycles;
+            })
+            .then(function (cycles) {
+                return $q.all(cycles.map(loadTotalsForCycle));
+            });
+
+        function loadTotalsForCycle(cycle) {
+            return cycle.getCycleSelectionAndInvoiceTotals()
+                .then(function (cycleTotals) {
+                    vm.totalsByCycle[cycle.id] = cycleTotals;
+                });
+        }
+    }
 }
