b033839af1266c40003a9e5fc9ab92226061579e
Marty <marty@pixotech.com>
2016-07-27 11:35:23 -0500

    CARLI-871 added library service and made form work similar to the staff app.

--- browserClient/libraryApp/sections/ipAddresses/ipAddresses.controller.js
+++ browserClient/libraryApp/sections/ipAddresses/ipAddresses.controller.js
@@ -1,68 +1,47 @@
 angular.module('library.sections.ipAddresses')
     .controller('ipAddressesController', ipAddressesController);
 
-function ipAddressesController( $q, $filter ){
+function ipAddressesController($q, $scope, libraryService, userService) {
     var vm = this;
-    vm.currentIPAddresses = [];
 
+    var currentUser = userService.getUser();
+    var currentLibrary = currentUser.library;
+
+    vm.library = null;
     vm.loadingPromise = null;
+
+    vm.cancelEdit = cancelEdit;
+    vm.save = save;
 
     activate();
 
-    function activate(){
-        initDataForControls();
-        initCurrentIPList();
+    function activate() {
+        initVmLibrary();
     }
 
-    function initCurrentIPList(){
-        vm.loadingPromise = TODO.listCurrentIPAddresses()
-            .then(function(ipAddressList){
-                vm.currentIPAddresses = ipAddressList;
+    function initVmLibrary() {
+        vm.loadingPromise = libraryService.load(currentLibrary.id)
+            .then(function (library) {
+                vm.library = library;
             });
         return vm.loadingPromise;
     }
 
-    function fetchSelectedProductsReportData(){
-        vm.loadingPromise = $q.all( vm.selectedCycles.map(listSelections) )
-            .then(sortResultsByCycleYear)
-            .then(combineReportOfferings);
+    function cancelEdit() {
+        return initVmLibrary()
+            .then(setFormPristine);
+    }
 
+    function save() {
+        vm.loadingPromise = libraryService.update(vm.library)
+            .then(initVmLibrary)
+            .then(setFormPristine);
         return vm.loadingPromise;
+    }
 
-        function listSelections(cycle){
-            return cycleService.listSelectionsForCycle(cycle, currentLibrary.id);
-        }
-
-        function sortResultsByCycleYear(listOfResultArrays){
-            return listOfResultArrays.sort(descendingChronologicalOrder);
-
-            function descendingChronologicalOrder(listA, listB){
-                return listB[0].cycle.year - listA[0].cycle.year;
-            }
-        }
-
-        function combineReportOfferings(listOfResultArrays){
-            var results = [];
-
-            listOfResultArrays.forEach(addListOfOfferingsToResults);
-
-            function addListOfOfferingsToResults(listOfResults){
-                $filter('orderBy')(listOfResults, 'product.name');
-
-                listOfResults.forEach(function(offering){
-                    results.push({
-                        cycle: offering.cycle.name,
-                        product: productService.getProductDisplayName(offering.product),
-                        vendor: offering.product.vendor.name,
-                        isFunded: offeringService.isFunded(offering) ? 'true' : 'false',
-                        selection: offering.selection.users,
-                        price: offeringService.getFundedSelectionPrice(offering),
-                        comment: offering.libraryComments
-                    });
-                });
-            }
-
-            return results;
+    function setFormPristine(){
+        if ($scope.ipEditForm) {
+            $scope.ipEditForm.$setPristine();
         }
     }
 }
