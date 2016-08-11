4be341ddfdfad090ad1ced0f091311c8af0e34bc
Bryan <bryan@pixotech.com>
2016-08-11 10:50:19 -0500

    Convert a few viewEdit templates that I missed
    CARLI-873

--- browserClient/common/components/viewEditDirectives/viewEditDate/viewEditDate.directive.js
+++ browserClient/common/components/viewEditDirectives/viewEditDate/viewEditDate.directive.js
@@ -1,8 +1,25 @@
 angular.module('common.viewEditDirectives.viewEditDate')
-    .directive('viewEditDate', function() {
+    .directive('viewEditDate', function () {
         return {
             restrict: 'E',
-            templateUrl: '/carliApp/components/viewEditDirectives/viewEditDate/viewEditDate.html',
+            template: [
+                '<div ng-show="editMode">',
+                '  <input type="text"',
+                '    datepicker-popup="{{format}}"',
+                '    ng-model="ngModel"',
+                '    is-open="opened"',
+                '    show-weeks="false"',
+                '    show-button-bar="false"',
+                '    min-date="minDate"',
+                '    max-date="maxDate"',
+                '    datepicker-options="dateOptions"',
+                '    close-text="Close"',
+                '    id="{{ inputId }}" />',
+                '  <div class="warning">Invalid date will not be saved.</div>',
+                '  <fa name="calendar" ng-click="open($event)"></fa>',
+                '</div>',
+                '<div ng-show="!editMode" ng-transclude></div>'
+            ].join(''),
             scope: {
                 ngModel: '=',
                 editMode: '=',
