c4c2f5509f5b58afc8269069f7ef6caec819aa16
BDub & Marty <brianw@pixotech.com>
2018-05-16 13:53:31 -0500

    Fix grid and refine layout. show numbers as localized currency.

--- browserClient/react/vendorSiteLicensePricing/gridCellComponent/gridCell.js
+++ browserClient/react/vendorSiteLicensePricing/gridCellComponent/gridCell.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 const GridCell = ({cell}) => {
     return (
-        <div>CELL!</div>
+        <div className="grid-cell">{ printValue(cell.siteLicensePrice) }</div>
     );
 };
+
+export function printValue(siteLicensePrice) {
+    return typeof siteLicensePrice === 'number' ?
+        siteLicensePrice.toLocaleString('en', {
+            currency: 'USD',
+            minimumFractionDigits: 2
+        }) :
+        '-';
+}
 
 export default GridCell;
--- browserClient/react/vendorSiteLicensePricing/siteLicenseGridComponent/siteLicenseGrid.js
+++ browserClient/react/vendorSiteLicensePricing/siteLicenseGridComponent/siteLicenseGrid.js
@@ -1,24 +1,59 @@
 import React from 'react';
-import LibraryRow from "../libraryRowComponent/libraryRow";
+import GridCellContainer from '../gridCellComponent/gridCellContainer';
+import {getKeyForLibraryAndProduct} from "../../reducers/siteLicenseReducer";
 
 const SiteLicenseGrid = ({ libraries, products }) => {
+    const productGridColumns = products
+        .map(p => '150px')
+        .join(' ');
+
+    const productAreaStyle = {
+        gridTemplateColumns: productGridColumns
+    };
+
     return (
         <div className="site-license-grid">
-            <div>
-                Library
+            <div className="site-license-grid__library-column">
+                <div className="site-license-grid__library-column-header">
+                    Library
+                </div>
+                { libraries.map(library => {
+                    return (
+                        <div className="site-license-grid__library-label" key={library.crmId}>{library.name}</div>
+                    );
+                })}
             </div>
-            { products.map(p => {
-                return (
-                    <div key={p.id}>{p.name}</div>
-                );
-            })}
-            { libraries.map(l => {
-                return (
-                    <LibraryRow library={l} products={products} key={l.crmId} />
-                );
-            })}
+            <div className="site-license-grid__product-area" style={productAreaStyle}>
+                { products.map(p => {
+                    return (
+                        <div className="site-license-grid__product-header" key={p.id}>{p.name}</div>
+                    );
+                })}
+                { gridCells(libraries, products) }
+            </div>
         </div>
     );
 };
+
+function gridCells(libraries, products) {
+    const gridCells = [];
+
+    libraries.forEach(l => {
+        products.forEach(p => {
+            gridCells.push(makeGridCell(l, p));
+        });
+    });
+
+    return gridCells;
+}
+
+function makeGridCell(library, product) {
+    return (
+        <GridCellContainer
+            library={library}
+            product={product}
+            key={getKeyForLibraryAndProduct(library, product)}/>
+    );
+}
 
 export default SiteLicenseGrid;
