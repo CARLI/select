113b10031730c35d673c8406385d59dcb2fbab98
BDub & Marty <brianw@pixotech.com>
2018-05-16 14:56:52 -0500

    Grid cells now have an edit mode and input that gets focused when activated

--- browserClient/react/vendorSiteLicensePricing/gridCellComponent/gridCell.js
+++ browserClient/react/vendorSiteLicensePricing/gridCellComponent/gridCell.js
@@ -1,18 +1,76 @@
 import React from 'react';
 
-const GridCell = ({cell}) => {
-    return (
-        <div className="grid-cell">{ printValue(cell.siteLicensePrice) }</div>
-    );
+export default class GridCell extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            editMode: false
+        };
+
+        this.inputRef = React.createRef();
+    }
+
+    componentDidUpdate() {
+        if (this.state.editMode)
+            this.inputRef.current.focus();
+    }
+
+    render() {
+        return (
+            <div className="grid-cell">
+                { this.displayMode() }
+                { this.editMode() }
+            </div>
+        );
+    }
+
+    displayMode() {
+        const style = {
+            display: this.state.editMode ? 'none' : 'block'
+        };
+
+        return (
+            <div className="grid-cell__display-mode" style={style}>
+                { GridCell.printValue(this.props.cell.siteLicensePrice) }
+                <button
+                    className="grid-cell__edit-button"
+                    type="button"
+                    onClick={this.setEditMode.bind(this)}>
+
+                    <span className="grid-cell__edit-button-text">Edit</span>
+                </button>
+            </div>
+        );
+    }
+
+    editMode() {
+        const style = {
+            display: this.state.editMode ? 'block' : 'none'
+        };
+
+        return (
+            <input
+                style={style}
+                ref={this.inputRef}
+                className="grid-cell__edit-input"
+                type="text"
+                defaultValue={this.props.cell.siteLicensePrice} />
+        );
+    }
+
+    static printValue(siteLicensePrice) {
+        return typeof siteLicensePrice === 'number' ?
+            siteLicensePrice.toLocaleString('en', {
+                currency: 'USD',
+                minimumFractionDigits: 2
+            }) :
+            '-';
+    }
+
+    setEditMode() {
+        this.setState({
+            editMode: true
+        });
+    }
 };
-
-export function printValue(siteLicensePrice) {
-    return typeof siteLicensePrice === 'number' ?
-        siteLicensePrice.toLocaleString('en', {
-            currency: 'USD',
-            minimumFractionDigits: 2
-        }) :
-        '-';
-}
-
-export default GridCell;
