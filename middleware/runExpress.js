ca1272f1a9d7796f51a7f8fadb656d3c1c372c8f
Bryan Cribbs <bryan@pixotech.com>
2018-12-10 16:16:47 -0600

    Cannot use fat arrow on the old production server.

--- middleware/runExpress.js
+++ middleware/runExpress.js
@@ -25,7 +25,7 @@ var vendorPricingCsv = require('./components/csv/vendorPricingCsv');
 var vendorReportCsv = require('./components/csv/vendorReport');
 var vendorSpecificProductQueries = require('./components/vendorSpecificProductQueries');
 var publicApi = require('./components/public');
-var consortiaManagerApi = require('./components/consortia-manager');
+var consortiaManagerApi = require('./components/restricted-api');
 
 function runMiddlewareServer(){
     var carliMiddleware = express();
