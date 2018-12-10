99e32c905a349f4684e7e10e69d86df14163dd02
Bryan Cribbs <bryan@pixotech.com>
2018-12-10 14:13:23 -0600

    Add endpoints for list-cycles and subscription-data

--- middleware/components/consortia-manager.js
+++ middleware/components/consortia-manager.js
@@ -1,5 +1,7 @@
-const libraryRepository = require('../../CARLI/Entity/LibraryRepository.js');
-const userRepository = require('../../CARLI/Entity/UserRepository.js');
+const cycleRepository = require('../../CARLI/Entity/CycleRepository');
+const libraryRepository = require('../../CARLI/Entity/LibraryRepository');
+const offeringRepository = require('../../CARLI/Entity/OfferingRepository');
+const userRepository = require('../../CARLI/Entity/UserRepository');
 
 const Q = require('q');
 
@@ -63,7 +65,82 @@ function listLibraryUsers() {
     }
 }
 
+function listCycles() {
+    return cycleRepository.list()
+        .then(translateCycles);
+
+    function translateCycles(cycles) {
+        return cycles.map(translateCycle)
+    }
+
+    function translateCycle(cycle) {
+        return {
+            id: cycle.id,
+            idalId: cycle.idalId,
+            name: cycle.name,
+            cycleType: cycle.cycleType,
+            year: cycle.year,
+            isArchived: cycle.isArchived,
+            startDateForSelections: cycle.startDateForSelections,
+            endDateForSelections: cycle.endDateForSelections,
+            productsAvailableDate: cycle.productsAvailableDate,
+            databaseName: cycle.databaseName
+        };
+    }
+}
+
+function getSubscriptionData(cycle) {
+    if (typeof cycle === 'undefined' || !cycle)
+        throw "Invalid cycle database name: Use /cm/list-cycles to list valid cycles";
+
+    var uniqueKeys = [];
+
+    return cycleRepository.load(cycle)
+        .then(offeringRepository.listOfferingsWithSelectionsUnexpanded)
+        .then(translateOfferings)
+        .then((foo) => {
+            console.log(uniqueKeys);
+            return foo;
+        });
+
+    function translateOfferings(offerings) {
+        return offerings.map(translateOffering);
+    }
+
+    function translateOffering(offering) {
+        if (offering.hasOwnProperty("selection")) {
+            const k = offering.selection.users;
+            if (uniqueKeys.indexOf(k) < 0)
+                uniqueKeys.push(k);
+        }
+        return {
+            product: offering.product,
+            vendor: offering.vendorId,
+            library: offering.library,
+            selection: offering.selection,
+            pricing: offering.pricing,
+        };
+    }
+
+    function deleteStuff(results) {
+        return results.map(row => {
+            delete row._id;
+            delete row._rev;
+            delete row.display;
+            delete row.libraryComments;
+            delete row.vendorComments;
+            delete row.internalComments;
+            delete row.cycle;
+            delete row.history;
+            delete row["$$hashKey"];
+            return row;
+        });
+    }
+}
+
 module.exports = {
     listLibraries: listLibraries,
-    listLibraryUsers: listLibraryUsers
+    listLibraryUsers: listLibraryUsers,
+    listCycles: listCycles,
+    getSubscriptionData: getSubscriptionData
 };
