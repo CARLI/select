97b07d998c4b3c77aeb691aa9808bb14a3bf6429
Ben Young <ben@pixotech.com>
2019-04-26 10:32:58 -0500

    Make uuid.service::generateCssId return an actual UUID
    The integers being returned by this method could cause ID
    collisions that led to odd form behavior. Using real UUIDs
    should fix this issue.
    
    CS-11

--- browserClient/common/components/uuid/uuid.service.js
+++ browserClient/common/components/uuid/uuid.service.js
@@ -1,10 +1,10 @@
+const uuid = require('uuid/v4');
+
 angular.module('common.uuid')
     .factory('uuid', function() {
-        var uniqueId = 0;
-
         return {
             generateCssId: function () {
-                return uniqueId++;
+                return uuid();
             }
         };
     });
