723786c60a652ed1449ed51f71c46e90d4bbee9c
Ben and Bryan <ben@pixotech.com>
2019-07-08 13:56:57 -0500

    Skip libraries with no notifications when generating invoices
    CS-33

--- CARLI/Banner.js
+++ CARLI/Banner.js
@@ -39,12 +39,6 @@ function getDataForBannerExportForSubscriptionCycle(cycle, batchId, formatter) {
         var offeringPromises = [];
         var dataForBatch = {};
 
-        notifications.forEach(n => {
-            if (typeof n === 'undefined') {
-                console.log("Notification is undefined");
-            }
-        });
-
         notifications.forEach(gatherDataForBatch);
 
         return Q.all(offeringPromises).then(function () {
@@ -53,8 +47,7 @@ function getDataForBannerExportForSubscriptionCycle(cycle, batchId, formatter) {
 
         function gatherDataForBatch(notification) {
             if (typeof notification == "undefined") {
-                console.log("Skipping inexplicably undefined notification");
-                return;
+                throw new Error("unexpected undefined notification");
             }
 
             if (notification.batchId != batchId) {
@@ -200,7 +193,8 @@ function getDataForBannerExportForMembershipDues(year, batchId, formatter) {
 
             var notification = invoicesForBatchByLibraryId[library.id];
             if (typeof notification === 'undefined') {
-                console.log(`Banner feed generator: Skipping ${library.name} because no invoice data is present.`)
+                console.log(`Banner feed generator: Skipping ${library.name} because no invoice data is present.`);
+                return
             }
             var dues = membershipDuesByLibraryId[library.id];
 
