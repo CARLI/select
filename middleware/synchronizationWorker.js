6603737700320e450ffb14ca72c71e0b7af975f7
Ben Young <ben@pixotech.com>
2020-01-27 10:21:08 -0600

    Do synchronization work as administrator.

--- middleware/synchronizationWorker.js
+++ middleware/synchronizationWorker.js
@@ -1,12 +1,16 @@
 
 var cluster = require('cluster');
 
+var databaseAuth = require('../CARLI/DatabaseAuth');
 var vendorDatabases = require('./components/vendorDatabases');
 
 Logger.log('Synchronization worker started');
 
-vendorDatabases.syncEverything()
-    .then(exitWorker);
+databaseAuth.asCouchAdmin(vendorDatabases.syncEverything)
+    .then(exitWorker)
+    .catch((err) => {
+        console.log("Synchronization worker failed", err);
+    });
 
 function exitWorker() {
     cluster.worker.kill();
