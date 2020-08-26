579ef0ac6c1a94253183b9bd1d42f58cd0cf2a92
Matthew Fotzler <mattf@pixotech.com>
2020-08-26 15:48:23 -0500

    Add support for credits on subscription pricing
    CARLI21-7

--- browserClient/common/components/viewEditDirectives/viewEditPrice/viewEditPrice.directive.js
+++ browserClient/common/components/viewEditDirectives/viewEditPrice/viewEditPrice.directive.js
@@ -4,7 +4,7 @@ angular.module('common.viewEditDirectives.viewEditPrice')
             restrict: 'E',
             template: [
               '<div ng-show="editMode" ng-class="{ \'is-funded\': fundedPrice }">',
-              '  <input class="full-price" type="number" step=".01" min="0" ng-model="ngModel" id="{{ inputId }}">',
+              '  <input class="full-price" type="number" step=".01" ng-model="ngModel" id="{{ inputId }}">',
               '  <span class="funded-price" ng-if="fundedPrice"><span class="funded-label">Funded Price</span>: {{ fundedPrice | currency }}</span>',
               '</div>',
               '<div ng-show="!editMode" ng-transclude></div>'
