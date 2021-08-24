0396595fb47f14833cb1e2b94d11414e266027ec
Ben Young <ben@pixotech.com>
2021-08-24 16:05:25 -0500

    Print a list of duplicate offerings instead of deleting

--- bin/delete-duplicate-offerings-not-in-vendor-db.js
+++ bin/delete-duplicate-offerings-not-in-vendor-db.js
@@ -1,14 +1,15 @@
 #!/usr/bin/env node
 
-console.log("This one-off script has already been run on production and is only retained for future reference.");
-process.exit();
+// console.log("This one-off script has already been run on production and is only retained for future reference.");
+// process.exit();
 
 // https://jira.pixotech.com/browse/CS-27
 // Ovid: LWW Nursing & Health Professions Premier Collection
 // const theAffectedProductId = "d45cc077-cf52-4bff-a504-a309326bc6db";
 // ProQuest: The Complete History in Video Package
-const theAffectedProductId = "87d1d984-ed3a-42b6-b23b-b79fb2362579";
-const fiscalYear2020Id = "548373da-5ac8-4d89-8a15-ff483be25822";
+const theAffectedProductId = "c2b9d303-f921-454b-b0e3-0a66f4caf990";
+// const fiscalYear2020Id = "5eaf42ea-d2b6-46f3-94dd-5eab010b9e13";
+const fiscalYear2020Id = "94822de1-5b6b-41e0-b650-fca78b7d2e5b";
 
 var _ = require('lodash');
 var path = require('path');
@@ -16,7 +17,9 @@ var Q = require('q');
 
 var cli = require('../CARLI/CommandLine');
 var cycleRepository = require('../CARLI/Entity/CycleRepository');
+var getCycleForVendor = require('../CARLI/Entity/CycleRepositoryForVendor');
 var offeringRepository = require('../CARLI/Entity/OfferingRepository');
+var vendorRepository = require('../CARLI/Entity/VendorRepository');
 
 listDuplicateOfferingsForCycle(fiscalYear2020Id);
 
@@ -90,7 +93,8 @@ function listDuplicateOfferingsForCycle(cycleId) {
                     libraryId: libraryId,
                     offeringId: offering.id,
                     pricing: offering.pricing,
-                    display: offering.display
+                    display: offering.display,
+                    vendorId: offering.vendorId
                 };
 
                 offeringsByProductPlusLibrary[trackId] = offeringsByProductPlusLibrary[trackId] || [];
@@ -109,12 +113,35 @@ function listDuplicateOfferingsForCycle(cycleId) {
         }
 
         function deleteOfferingsNotInVendorDatabase(listOfDuplicateTrackObjects) {
-            const offeringsInVendorDatabase = [];
-
             // flatten the duplicates into a single list
+            const flatDuplicates = [];
+            listOfDuplicateTrackObjects.map(list => list.map(duplicate => flatDuplicates.push(duplicate)));
+            //Logger.log(JSON.stringify(flatDuplicates));
+            return Q.allSettled(flatDuplicates.map(async duplicate => {
+                const expanded = await offeringRepository.load(duplicate.offeringId, currentCycle);
+                return {
+                    id: expanded.id,
+                    library: expanded.library.name,
+                    product: expanded.product.name,
+                    site: expanded.pricing.site,
+                    su: expanded.pricing.su
+                };
+            })).then(items => Logger.log(JSON.stringify(items.map(x => x.value).sort((a, b) => a.library < b.library ? -1 : 1))));
             // delete every offering not in the vendor database
-            var promises;
-            return Q.allSettled(promises);
+            // var promises = flatDuplicates.map(async duplicate => {
+            //     // get offerings from vendor database (?)
+            //     const vendorCycle = await getCycleForVendor({ id: duplicate.vendorId }).load(currentCycle.id);
+            //     const vendorOfferings = await offeringRepository.listOfferingsForProductIdUnexpanded(duplicate.productId, vendorCycle);
+            //
+            //     // if duplicate not in vendor database
+            //     if (vendorOfferings.find(offering => offering.id === duplicate.offeringId) === undefined) {
+            //         Logger.log(`Deleting offering ${duplicate.offeringId}`);
+            //         //   then delete duplicate from main database
+            //         // promises.push(offeringRepository.delete(duplicate.offeringId, currentCycle));
+            //     }
+            // });
+            // return Q.allSettled(promises);
+            //return Q.allSettled([]);
         }
 
         function deleteOfferingsWithNoPriceData(listOfDuplicateTrackObjects) {
