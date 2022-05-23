a5541513a86351583900ce0facea3d81fb781954
Matthew Fotzler <mattf@pixotech.com>
2022-05-23 11:12:20 -0500

    Fix bug where every contact was being treated as a Sales contact

--- browserClient/libraryApp/components/vendorSalesContact/vendorSalesContact.controller.js
+++ browserClient/libraryApp/components/vendorSalesContact/vendorSalesContact.controller.js
@@ -22,7 +22,7 @@ function vendorSalesContactController() {
         }
 
         function salesContact(contact) {
-            return contact.contactType = 'Sales';
+            return contact.contactType === 'Sales';
         }
     }
 }
