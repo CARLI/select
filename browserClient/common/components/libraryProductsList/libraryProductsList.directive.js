14646c9d4e725e7bb3fdad0d92aa7693b6b506e1
Ben & Matt & Andrew <mattf@pixotech.com>
2023-06-13 17:02:37 -0500

    Add an almaId field to products and replace all instances of comments with the new field
    CARLI-102

--- browserClient/common/components/libraryProductsList/libraryProductsList.directive.js
+++ browserClient/common/components/libraryProductsList/libraryProductsList.directive.js
@@ -1,52 +1,52 @@
 angular.module('common.libraryProductsList')
-.directive('libraryProductsList', function(){
-    return {
-        restrict: 'E',
-        template: [
-            '<ul class="product-list selected-products" cg-busy="vm.loadingPromise">',
-            '    <li class="column-headers">',
-            '        <div class="sortable column product" ng-class="{ activeSort: vm.orderBy === vm.sortOptions.productName, reversedSort: vm.reverse }" ng-click="vm.sort(vm.sortOptions.productName)">Product</div>',
-            '        <div class="sortable column vendor" ng-class="{ activeSort: vm.orderBy === vm.sortOptions.vendorName, reversedSort: vm.reverse }" ng-click="vm.sort(vm.sortOptions.vendorName)">Vendor</div>',
-            '        <div class="sortable column license" ng-class="{ activeSort: vm.orderBy === vm.sortOptions.license, reversedSort: vm.reverse }" ng-click="vm.sort(vm.sortOptions.license)">License Agreement</div>',
-            '        <div class="column comment">Comments</div>',
-            '        <div class="sortable column su" ng-class="{ activeSort: vm.orderBy === vm.sortOptions.su, reversedSort: vm.reverse }" ng-click="vm.sort(vm.sortOptions.su)">S.U.</div>',
-            '        <div class="sortable column cost" ng-class="{ activeSort: vm.orderBy === vm.sortOptions.cost, reversedSort: vm.reverse }" ng-click="vm.sort(vm.sortOptions.cost)">Cost</div>',
-            '    </li>',
-            '    <li class="product" ng-repeat="offering in vm.selectedOfferings | orderBy: vm.orderBy:vm.reverse track by offering.id">',
-            '        <div class="column product">{{ vm.getProductDisplayName(offering.product) }}</div>',
-            '        <div class="column vendor">{{ offering.product.vendor.name }}</div>',
-            '        <div class="column license" data-license-id="{{ offering.product.license.id }}">{{ offering.product.license.name }}</div>',
-            '        <div class="column comment">{{ offering.libraryComments || offering.product.comments }}</div>',
-            '        <div class="column su">{{ offering.selection.users }}</div>',
-            '        <div class="column cost">{{ vm.getFundedSelectionPrice(offering) | currency }}</div>',
-            '    </li>',
-            '    <li class="summary-row">',
-            '        <div class="column"><span class="total-label">Total</span></div>',
-            '        <div class="column cost">{{ vm.selectionTotal() | currency }}</div>',
-            '    </li>',
-            '</ul>',
-            '<button class="carli-button" type="button" busy-click="vm.exportProductList()">Export</button>'
-        ].join(''),
-        scope: {
-            cycle: '=',
-            libraryId: '@',
-            licenseOnClick: '='
-        },
-        controller: libraryProductsListController,
-        controllerAs: 'vm',
-        bindToController: true,
-        link: libraryProductsListLink
-    };
+    .directive('libraryProductsList', function () {
+        return {
+            restrict: 'E',
+            template: [
+                '<ul class="product-list selected-products" cg-busy="vm.loadingPromise">',
+                '    <li class="column-headers">',
+                '        <div class="sortable column product" ng-class="{ activeSort: vm.orderBy === vm.sortOptions.productName, reversedSort: vm.reverse }" ng-click="vm.sort(vm.sortOptions.productName)">Product</div>',
+                '        <div class="sortable column vendor" ng-class="{ activeSort: vm.orderBy === vm.sortOptions.vendorName, reversedSort: vm.reverse }" ng-click="vm.sort(vm.sortOptions.vendorName)">Vendor</div>',
+                '        <div class="sortable column license" ng-class="{ activeSort: vm.orderBy === vm.sortOptions.license, reversedSort: vm.reverse }" ng-click="vm.sort(vm.sortOptions.license)">License Agreement</div>',
+                '        <div class="column comment">Alma CZ E-Collection ID</div>',
+                '        <div class="sortable column su" ng-class="{ activeSort: vm.orderBy === vm.sortOptions.su, reversedSort: vm.reverse }" ng-click="vm.sort(vm.sortOptions.su)">S.U.</div>',
+                '        <div class="sortable column cost" ng-class="{ activeSort: vm.orderBy === vm.sortOptions.cost, reversedSort: vm.reverse }" ng-click="vm.sort(vm.sortOptions.cost)">Cost</div>',
+                '    </li>',
+                '    <li class="product" ng-repeat="offering in vm.selectedOfferings | orderBy: vm.orderBy:vm.reverse track by offering.id">',
+                '        <div class="column product">{{ vm.getProductDisplayName(offering.product) }}</div>',
+                '        <div class="column vendor">{{ offering.product.vendor.name }}</div>',
+                '        <div class="column license" data-license-id="{{ offering.product.license.id }}">{{ offering.product.license.name }}</div>',
+                '        <div class="column comment">{{ offering.product.almaId }}</div>',
+                '        <div class="column su">{{ offering.selection.users }}</div>',
+                '        <div class="column cost">{{ vm.getFundedSelectionPrice(offering) | currency }}</div>',
+                '    </li>',
+                '    <li class="summary-row">',
+                '        <div class="column"><span class="total-label">Total</span></div>',
+                '        <div class="column cost">{{ vm.selectionTotal() | currency }}</div>',
+                '    </li>',
+                '</ul>',
+                '<button class="carli-button" type="button" busy-click="vm.exportProductList()">Export</button>'
+            ].join(''),
+            scope: {
+                cycle: '=',
+                libraryId: '@',
+                licenseOnClick: '='
+            },
+            controller: libraryProductsListController,
+            controllerAs: 'vm',
+            bindToController: true,
+            link: libraryProductsListLink
+        };
 
-    function libraryProductsListLink( scope, element, attrs, controller ){
-        element.on('click', '.product-list li.product .column.license', function(e){
-            var licenseId = $(this).data('licenseId');
-            if ( controller.licenseOnClick ){
-                scope.$apply(function(){
-                    controller.licenseOnClick(licenseId);
-                });
-            }
-        });
-    }
+        function libraryProductsListLink(scope, element, attrs, controller) {
+            element.on('click', '.product-list li.product .column.license', function (e) {
+                var licenseId = $(this).data('licenseId');
+                if (controller.licenseOnClick) {
+                    scope.$apply(function () {
+                        controller.licenseOnClick(licenseId);
+                    });
+                }
+            });
+        }
 
-});
+    });
